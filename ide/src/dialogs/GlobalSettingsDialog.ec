ecere/ecere-sdk

commit 6baa12647265b610401b943e2a7c86d26b46c59e

ide/Build System: Improved when makefiles/compiler configs get regenerated (Makefiles no longer get generated when switching compiler)

ide/src/dialogs/GlobalSettingsDialog.ec
@@ -88,11 +88,19 @@ class GlobalSettingsDialog : Window
             
             if(compilersTab.modifiedDocument)
             {
+               bool foundActive = false;
                if(strcmp(compilersTab.compilerConfigsDir.path, ideSettings.compilerConfigsDir))
                   ideSettings.compilerConfigsDir = compilersTab.compilerConfigsDir.path;
                ideSettings.compilerConfigs.Free();
                for(compiler : compilersTab.compilerConfigs)
+               {
                   ideSettings.compilerConfigs.Add(compiler.Copy());
+                  if(!foundActive && ide.workspace.compiler && !strcmp(ide.workspace.compiler, compiler.name))
+                     foundActive = true;
+               }
+               if(!foundActive)
+                  ide.workspace.compiler = defaultCompilerName;
+
                compilerSettingsChanged = true;
             }
 

ide/src/dialogs/ActiveCompilerDialog.ec
@@ -49,14 +49,12 @@ class ActiveCompilerDialog : Window
 
    bool OnPostCreate()
    {
-      DataRow row;
       for(compiler : ideSettings.compilerConfigs)
       {
-         row = compilerDropBox.AddString(compiler.name);
+         DataRow row = compilerDropBox.AddString(compiler.name);
          if(workspaceActiveCompiler && !strcmp(compiler.name, workspaceActiveCompiler))
             compilerDropBox.currentRow = row;
       }
       return true;
    }
-   
 }

ide/src/dialogs/NewProjectDialog.ec
@@ -194,7 +194,9 @@ class NewProjectDialog : Window
             CompilerConfig compiler = ideSettings.GetCompilerConfig(ide.workspace.compiler);
             ProjectConfig config = project.config;
             projectWindow.ShowOutputBuildLog(true);
-            projectWindow.ProjectPrepareMakefile(project, force, true, true, compiler, config);
+            projectWindow.DisplayCompiler(compiler, false);
+            projectWindow.ProjectPrepareCompiler(project, compiler);
+            projectWindow.ProjectPrepareMakefile(project, force, compiler, config);
             delete compiler;
          }
 
@@ -510,7 +512,9 @@ class QuickProjectDialog : Window
             CompilerConfig compiler = ideSettings.GetCompilerConfig(ide.workspace.compiler);
             ProjectConfig config = project.config;
             projectWindow.ShowOutputBuildLog(true);
-            projectWindow.ProjectPrepareMakefile(project, force, true, true, compiler, config);
+            projectWindow.DisplayCompiler(compiler, false);
+            projectWindow.ProjectPrepareCompiler(project, compiler);
+            projectWindow.ProjectPrepareMakefile(project, force, compiler, config);
             delete compiler;
          }
 

ide/src/ide.ec
@@ -171,7 +171,7 @@ GlobalSettingsDialog globalSettingsDialog
             break;
          case compilerSettings:
          {
-            ide.UpdateMakefiles();
+            ide.UpdateCompilerConfigs();
             break;
          }
       }
@@ -1544,10 +1544,21 @@ class IDEWorkSpace : Window
       if(workspace)
       {
          CompilerConfig compiler = ideSettings.GetCompilerConfig(workspace.compiler);
+         for(prj : workspace.projects)
+            projectView.ProjectUpdateMakefileForAllConfigs(prj);
+         delete compiler;
+      }
+   }
+
+   void UpdateCompilerConfigs()
+   {
+      if(workspace)
+      {
+         CompilerConfig compiler = ideSettings.GetCompilerConfig(workspace.compiler);
          projectView.ShowOutputBuildLog(true);
          projectView.DisplayCompiler(compiler, false);
          for(prj : workspace.projects)
-            projectView.ProjectUpdateMakefileForAllConfigs(prj);
+            projectView.ProjectPrepareCompiler(prj, compiler);
          delete compiler;
       }
    }
@@ -1838,6 +1849,7 @@ class IDEWorkSpace : Window
                            ide.projectView.DisplayCompiler(compiler, false);
                            delete compiler;
                         }
+                        UpdateCompilerConfigs();
                         UpdateMakefiles();
                         {
                            char newWorkingDir[MAX_LOCATION];

ide/src/project/Project.ec
@@ -44,7 +44,6 @@ IDESettingsContainer settingsContainer
 #ifndef MAKEFILE_GENERATOR
       globalSettingsDialog.ideSettings = settings;
       ide.UpdateRecentMenus();
-      // ide.UpdateMakefiles(); -- can't really regenerate on Load since all recent menus changes will happen
 #endif
    }
 };

ide/src/project/ProjectView.ec
@@ -506,12 +506,7 @@ class ProjectView : Window
          {
             prj.StopMonitoring();
             if(prj.Save(prj.filePath))
-            {
-               // ShowOutputBuildLog(true);
-               // DisplayCompiler(compiler, false);
-               // ProjectUpdateMakefileForAllConfigs(prj);
                prj.topNode.modified = false;
-            }
             prj.StartMonitoring();
          }
       }
@@ -574,69 +569,80 @@ class ProjectView : Window
       if(displayCompiler)
          DisplayCompiler(compiler, false);
 
-      ProjectPrepareMakefile(project, method, false, false, compiler, config);
+      ProjectPrepareCompiler(project, compiler);
+      ProjectPrepareMakefile(project, method, compiler, config);
       return true;
    }
 
-   bool ProjectPrepareMakefile(Project project, PrepareMakefileMethod method, bool cleanLog, bool displayCompiler,
-      CompilerConfig compiler, ProjectConfig config)
+   bool ProjectPrepareCompiler(Project project, CompilerConfig compiler)
    {
-      char makefilePath[MAX_LOCATION];
-      char makefileName[MAX_LOCATION];
-      bool exists;
-      LogBox logBox = ide.outputView.buildBox;
-      
-      if(displayCompiler)
-         DisplayCompiler(compiler, false);
+      project.GenerateCrossPlatformCf();
+      project.GenerateCompilerMk(compiler);
+      return true;
+   }
 
-      strcpy(makefilePath, project.topNode.path);
-      project.CatMakeFileName(makefileName, config);
-      PathCatSlash(makefilePath, makefileName);
-
-      exists = FileExists(makefilePath);
-      if((method == normal && (!exists || config.makingModified/*|| project.topNode.modified*/)) ||
-            (method == forceExists && exists) || 
-            method == force) // || config.makingModified || makefileDirty
-      {
-         char * reason;
-         char * action;
-         ide.statusBar.text = $"Generating Makefile & Dependencies..."; // Dependencies?
-         app.UpdateDisplay();
-         
-         if((method == normal && !exists) || (method == force && !exists))
-            action = $"Generating ";
-         else if(method == force)
-            action = $"Regenerating ";
-         else if(method == normal || method == forceExists)
-            action = $"Updating ";
-         else
-            action = "";
-         if(!exists)
-            reason = $"Makefile doesn't exist. ";
-         else if(project.topNode.modified)
-            reason = $"Project has been modified. ";
-         else
-            reason = "";
-
-         //logBox.Logf("%s\n", makefileName);
-         logBox.Logf($"%s - %s%smakefile for %s config...\n", makefileName, reason, action, GetConfigName(config));
-
-         project.GenerateCrossPlatformCf();
-         project.GenerateCompilerMk(compiler);
-
+   // Note: Compiler is only passed in to for VisualStudio support
+   bool ProjectPrepareMakefile(Project project, PrepareMakefileMethod method, CompilerConfig compiler, ProjectConfig config)
+   {
 #if defined(__WIN32__)  // I'm guessing we'll want to support generating VS files on Linux as well...
-         if(compiler.type.isVC)
-         {
-            GenerateVSSolutionFile(project, compiler);
-            GenerateVCProjectFile(project, compiler);
-         }
-         else
-#endif
-            project.GenerateMakefile(null, false, null, config);
-
+      if(compiler.type.isVC)
+      {
+         ide.statusBar.text = $"Generating Visual Studio Solution...";
+         app.UpdateDisplay();
+         GenerateVSSolutionFile(project, compiler);
+         ide.statusBar.text = $"Generating Visual Studio Project...";
+         app.UpdateDisplay();
+         GenerateVCProjectFile(project, compiler);
          ide.statusBar.text = null;
          app.UpdateDisplay();
          return true;
+      }
+      else
+#endif
+      {
+         char makefilePath[MAX_LOCATION];
+         char makefileName[MAX_LOCATION];
+         bool exists;
+         LogBox logBox = ide.outputView.buildBox;
+         
+         strcpy(makefilePath, project.topNode.path);
+         project.CatMakeFileName(makefileName, config);
+         PathCatSlash(makefilePath, makefileName);
+
+         exists = FileExists(makefilePath);
+         if(method == force ||
+           (method == forceExists && exists) ||
+           (method == normal && (!exists || config.makingModified)) )
+         {
+            char * reason;
+            char * action;
+            ide.statusBar.text = $"Generating Makefile & Dependencies..."; // Dependencies?
+            app.UpdateDisplay();
+
+            if((method == normal && !exists) || (method == force && !exists))
+               action = $"Generating ";
+            else if(method == force)
+               action = $"Regenerating ";
+            else if(method == normal || method == forceExists)
+               action = $"Updating ";
+            else
+               action = "";
+            if(!exists)
+               reason = $"Makefile doesn't exist. ";
+            else if(project.topNode.modified)
+               reason = $"Project has been modified. ";
+            else
+               reason = "";
+
+            //logBox.Logf("%s\n", makefileName);
+            logBox.Logf($"%s - %s%smakefile for %s config...\n", makefileName, reason, action, GetConfigName(config));
+
+            project.GenerateMakefile(null, false, null, config);
+
+            ide.statusBar.text = null;
+            app.UpdateDisplay();
+            return true;
+         }
       }
       return false;
    }
@@ -962,7 +968,9 @@ class ProjectView : Window
             prj = node.project;
       }
 
-      ProjectPrepareMakefile(prj, force, true, true, compiler, prj.config);
+      DisplayCompiler(compiler, false);
+      ProjectPrepareCompiler(project, compiler);
+      ProjectPrepareMakefile(prj, force, compiler, prj.config);
       delete compiler;
       return true;
    }
@@ -1152,10 +1160,7 @@ class ProjectView : Window
       CompilerConfig compiler = ideSettings.GetCompilerConfig(ide.workspace.compiler);
 
       for(config : project.configurations)
-      {
-         ProjectPrepareMakefile(project, forceExists, false, false,
-            compiler, config);
-      }
+         ProjectPrepareMakefile(project, forceExists, compiler, config);
 
       ide.Update(null);
       delete compiler;
@@ -1183,8 +1188,9 @@ class ProjectView : Window
          ide.workspace.compiler = compilerDialog.workspaceActiveCompiler;
          ide.projectView.ShowOutputBuildLog(true);
          ide.projectView.DisplayCompiler(compiler, false);
+
          for(prj : ide.workspace.projects)
-            ide.projectView.ProjectUpdateMakefileForAllConfigs(prj);
+            ide.projectView.ProjectPrepareCompiler(prj, compiler);
          delete compiler;
       }
       delete compilerDialog;
@@ -1529,9 +1535,6 @@ class ProjectView : Window
          if(prj.Save(prj.filePath))
          {
             Project modPrj = null;
-            // ShowOutputBuildLog(true);
-            // DisplayCompiler(compiler, false);
-            // ProjectUpdateMakefileForAllConfigs(prj);
             prj.topNode.modified = false;
             for(p : ide.workspace.projects)
             {

commit 8ceb55a7d9017950b7b12f804c89d3e22894539f

ide/GlobalSettingsDialog: Fixed a crash introduced in 6baa12647265b610401b943e2a7c86d26b46c59e when editing compilers and no workspace is opened

ide/src/dialogs/GlobalSettingsDialog.ec
@@ -89,17 +89,18 @@ class GlobalSettingsDialog : Window
             if(compilersTab.modifiedDocument)
             {
                bool foundActive = false;
+               Workspace workspace = ide.workspace;
                if(strcmp(compilersTab.compilerConfigsDir.path, ideSettings.compilerConfigsDir))
                   ideSettings.compilerConfigsDir = compilersTab.compilerConfigsDir.path;
                ideSettings.compilerConfigs.Free();
                for(compiler : compilersTab.compilerConfigs)
                {
                   ideSettings.compilerConfigs.Add(compiler.Copy());
-                  if(!foundActive && ide.workspace.compiler && !strcmp(ide.workspace.compiler, compiler.name))
+                  if(!foundActive && workspace && workspace.compiler && !strcmp(workspace.compiler, compiler.name))
                      foundActive = true;
                }
-               if(!foundActive)
-                  ide.workspace.compiler = defaultCompilerName;
+               if(workspace && !foundActive)
+                  workspace.compiler = defaultCompilerName;
 
                compilerSettingsChanged = true;
             }