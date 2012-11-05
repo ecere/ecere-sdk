c3ce3a14c07b63bd471bc8cdcf317a42c6fc4bbd
Jerome St-Louis <jerome@ecere.com>
2012-11-05 15:37:12 -0500

    sdk/Build System: Took out erroneous/superfluous stuff from include.mk; Added missing target platform support for tarball

--- include.mk
+++ include.mk
@@ -1,20 +1,11 @@
 # TOOLCHAIN
 export CC      = $(CCACHE_COMPILE)$(DISTCC_COMPILE)$(GCC_PREFIX)gcc$(_SYSROOT)
 export CPP     = $(CCACHE_COMPILE)$(DISTCC_COMPILE)$(GCC_PREFIX)cpp$(_SYSROOT)
-export ECP     = ecp
-export ECC     = ecc
-export ECS     = %s$(if $(CROSS_TARGET), -t $(TARGET_PLATFORM),)
-export EAR     = ear
 export AS      = $(GCC_PREFIX)as
 export LD      = $(GCC_PREFIX)ld
 export AR      = $(GCC_PREFIX)ar
 export STRIP   = $(GCC_PREFIX)strip
 UPX := upx
-
-# DEBIAN
-ifdef DEBIAN_PACKAGE
-CFLAGS += $(CPPFLAGS)
-endif
 
 ifeq "$(HOST_ARCH)" "ARM"
 FORCE_64_BIT =
