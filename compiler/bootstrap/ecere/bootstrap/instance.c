ecere/ecere-sdk

commit bb1fdba96476884a6b931bc6bef3e7b909602288

ecere/com: Solved class registration ordering issues - Resolved conditional jumps based on uninitialized values reported by Valgrind  (Was mistakenly comparing against 'foundation' class (class) virtual table for virtual methods not found in there) - Ultimately replaced that whole extra code by the section of code in the if block above (move it outside the if) - Tested with EceRIS CodedLocation>LocationAbbreviation>Location>Id structure and both orders of LocationAbbreviation/CodedLocation - Solved base type correction for enum base classes using code from RegisterClass

ecere/src/com/instance.ec
@@ -1896,14 +1896,27 @@ static void FixDerivativesBase(Class base, Class mod)
       int sizeClass = _class.sizeClass - _class.offsetClass;
       Class enumBase = null;
       char * dataTypeString = null;
+      // NOTE: baseClass is class(class)
       Class baseClass;
       uint offsetBefore = _class.offset;
 
       int offsetClass, totalSizeClass;
 
       for(baseClass = base; baseClass.base; baseClass = baseClass.base);
-      
-      if(base && !base.internalDecl && (base.type == noHeadClass || base.type == structClass || base.type == normalClass)) type = base.type;
+
+      if(base && !base.internalDecl && (base.type == noHeadClass || base.type == structClass || base.type == normalClass))
+      {
+         // Normal classes inheriting off simple classes should become no head classes
+         if(base.type == structClass && type == normalClass)
+            type = noHeadClass;
+         else
+            type = base.type;
+      }
+      if(base && (_class.type == normalClass || _class.type == noHeadClass || _class.type == structClass) &&
+         (base.type == unitClass || base.type == bitClass || base.type == enumClass))
+      {
+         type = base.type;
+      }
 
       if(type == enumClass)
       {
@@ -1992,6 +2005,7 @@ static void FixDerivativesBase(Class base, Class mod)
                if(method.type == virtualMethod)
                   method.vid += mod.base.vTblSize - baseClass.vTblSize;
             }
+         }
 
             for(b = mod.base; b && b != null; b = b.base)
             {
@@ -2023,8 +2037,10 @@ static void FixDerivativesBase(Class base, Class mod)
                   }
                }
             }
-         }
+         //}
 
+         // Trying to simply move out the above block of code outside the if to handle this
+         /*
          // Also doing this now, otherwise overridden methods of base classes from intermediate classes will not be set in higher level class
          // (e.g. OnGetString overridden in Id , Location inheriting from Id, LocationAbbreviation created later inheriting from Location would not get Id's OnGetString)
          for(b = mod.base; b && b != null; b = b.base)
@@ -2034,11 +2050,12 @@ static void FixDerivativesBase(Class base, Class mod)
             {
                if(vMethod.type == virtualMethod)
                {
-                  if(_class._vTbl[vMethod.vid] == baseClass._vTbl[vMethod.vid] && _class._vTbl[vMethod.vid] != _class.base._vTbl[vMethod.vid])
+                  if(_class._vTbl[vMethod.vid] == b._vTbl[vMethod.vid] && _class._vTbl[vMethod.vid] != _class.base._vTbl[vMethod.vid])
                      _class._vTbl[vMethod.vid] = _class.base._vTbl[vMethod.vid];
                }
             }
          }
+         */
       }
 
       // _class.defaultAlignment = base ? base.defaultAlignment : 0;

commit 91490c6d9affe0eafa046b8f59eecbf97dcec6fa

ecere/instance: Fixed issue with template virtual methods (e.g. AVLTree::Add) - Tweak to fix problem introduced by bb1fdba96476884a6b931bc6bef3e7b909602288

compiler/bootstrap/ecere/bootstrap/instance.c
@@ -1222,8 +1222,9 @@ _class->sizeClass = totalSizeClass;
 {
 struct __ecereNameSpace__ecere__com__Method * method, * next;
 struct __ecereNameSpace__ecere__com__Class * b;
+unsigned int needUpdate = (mod != (base->templateClass ? base->templateClass : base) || _class->vTblSize != mod->vTblSize);
 
-if(mod->base && mod->base->base && mod->base->vTblSize > baseClass->vTblSize && (mod != (base->templateClass ? base->templateClass : base) || _class->vTblSize != mod->vTblSize))
+if(mod->base && mod->base->base && mod->base->vTblSize > baseClass->vTblSize && needUpdate)
 {
 _class->vTblSize += mod->base->vTblSize - baseClass->vTblSize;
 _class->_vTbl = __ecereNameSpace__ecere__com__eSystem_Renew(_class->_vTbl, sizeof(void *) * (_class->vTblSize));
@@ -1262,7 +1263,7 @@ method->dataTypeString = __ecereNameSpace__ecere__sys__CopyString(vMethod->dataT
 method->_class = vMethod->_class;
 }
 }
-else
+else if(needUpdate || _class->_vTbl[vMethod->vid] == b->_vTbl[vMethod->vid])
 _class->_vTbl[vMethod->vid] = _class->base->_vTbl[vMethod->vid];
 }
 }
@@ -5089,8 +5090,6 @@ struct __ecereNameSpace__ecere__com__Instance * __ecereNameSpace__ecere__com____
 {
 struct __ecereNameSpace__ecere__com__Instance * app;
 
-__ecereNameSpace__ecere__com__memoryInitialized = 0x0;
-__ecereNameSpace__ecere__com__pools = (((void *)0));
 app = __ecereNameSpace__ecere__com___calloc(1, 428);
 __ecereNameSpace__ecere__com__Module_Constructor(app);
 ((struct __ecereNameSpace__ecere__com__Application *)(((char *)app + 300)))->systemNameSpace.classes.CompareKey = (void *)__ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_CompareString;

ecere/src/com/instance.ec
@@ -1990,9 +1990,9 @@ static void FixDerivativesBase(Class base, Class mod)
       {
          Method method, next;
          Class b;
+         bool needUpdate = (mod != (base.templateClass ? base.templateClass : base) || _class.vTblSize != mod.vTblSize;
 
-         if(mod.base && mod.base.base && mod.base.vTblSize > baseClass.vTblSize && 
-            (mod != (base.templateClass ? base.templateClass : base) || _class.vTblSize != mod.vTblSize))
+         if(mod.base && mod.base.base && mod.base.vTblSize > baseClass.vTblSize && needUpdate)
          {
             _class.vTblSize += mod.base.vTblSize - baseClass.vTblSize;
             _class._vTbl = renew _class._vTbl void *[_class.vTblSize];
@@ -2032,7 +2032,7 @@ static void FixDerivativesBase(Class base, Class mod)
                            method._class = vMethod._class;
                         }
                      }
-                     else
+                     else if(needUpdate || _class._vTbl[vMethod.vid] == b._vTbl[vMethod.vid])
                         _class._vTbl[vMethod.vid] = _class.base._vTbl[vMethod.vid];
                   }
                }