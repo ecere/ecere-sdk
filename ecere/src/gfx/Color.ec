ecere/ecere-sdk

commit 0faaa3a99957395bcb1a323bf18c8f0e1ffca642

ecere, eda: GUI Tweaks to fix autoSize behaviors

ecere/src/gui/controls/DataBox.ec
@@ -98,7 +98,8 @@ private:
 
    bool OnPostCreate()
    {
-      if(type && !readOnly && (type.type == normalClass || type.type == noHeadClass || data))
+      if(type && (!readOnly || (autoSize && type._vTbl[__ecereVMethodID_class_OnEdit] == class(Instance)._vTbl[__ecereVMethodID_class_OnEdit])) &&
+         (type.type == normalClass || type.type == noHeadClass || data))
       {
          // IMPORTANT FIX: If keepEditor is true, we were passing editor rather than the editor's current master
          editor = (Window)type._vTbl[__ecereVMethodID_class_OnEdit](type, 
@@ -115,7 +116,10 @@ private:
                PrintLn("DataBox::OnPostCreate -- autoSize == true");
 #endif*/
             if(eClass_IsDerived(editor._class, class(EditBox)))
+            {
+               ((EditBox)editor).readOnly = readOnly;
                ((EditBox)editor).autoSize = autoSize;
+            }
          }
          else
          {
@@ -129,7 +133,7 @@ private:
 
    void OnRedraw(Surface surface)
    {
-      if(type)
+      if(type && !editor)
       {
          char tempString[1024];
          if(type._vTbl[__ecereVMethodID_class_OnDisplay] == class(Instance)._vTbl[__ecereVMethodID_class_OnDisplay])

ecere/src/gui/controls/ListBox.ec
@@ -1072,6 +1072,7 @@ public:
 #ifdef _DEBUG
    private void CheckConsistency()
    {
+#if 0
       DataRow r;
       int index = 0;
       for(r = rows.first; r; r = r.GetNextRow())
@@ -1079,6 +1080,7 @@ public:
          if(r.index != index++)
             PrintLn("bug");
       }
+#endif
    }
 #endif
 

ecere/src/gui/typeEdit.ec
@@ -172,8 +172,7 @@ static Window OnEdit(Class _class, void * data, Window window, Window master,
          window, master = master, visible = false, //position = { x, y }, 
          borderStyle = 0,
          modifyVirtualArea = false, //sizeAnchor = { { w, h }, isClientH = true };
-         autoSize = (eClass_IsDerived(window._class, class(DataBox)) ? ((DataBox)window).autoSize : false),
-         anchor = { 0, 0, 0, 0 };
+         autoSize = (eClass_IsDerived(window._class, class(DataBox)) ? ((DataBox)window).autoSize : false);
 
          void DataBox::NotifyUpdate(EditBox editBox)
          {
@@ -183,10 +182,15 @@ static Window OnEdit(Class _class, void * data, Window window, Window master,
 
          bool OnActivate(bool active, Window previous, bool * goOnWithActivation, bool direct)
          {
-            opacity = active ? 1.0f : parent.opacity;
+            opacity = (active && !readOnly) ? 1.0f : parent.opacity;
             return EditBox::OnActivate(active, previous, goOnWithActivation, direct);
          }
       };
+      if(!editBox.autoSize)
+         editBox.anchor = { 0, 0, 0, 0 };
+      else
+         editBox.anchor = { 0, 0 };
+
       if(data)
       {
          bool needClass = false;

eda/libeda/src/gui/TableEditor.ec
@@ -1321,6 +1321,11 @@ private:
 
    void AddFieldBox(FieldBox fieldBox)
    {
+      // I was putting this here to force autosize on the lists (e.g. the Radiologists fields):
+      /*
+      if(!fieldBox.autoSize)
+         fieldBox.autoSize = true;
+      */
       DebugLn("TableEditor::AddFieldBox");
       if(!fieldsBoxes.Find(fieldBox))
       {
@@ -1420,6 +1425,7 @@ private:
          fb.Load();
       OnCreateDynamicLookupEditors();
       internalModifications = false;
+      size = size;   // This seems to be required to fix autoSize on entering order screen
 
       DebugLn("   TODO: implement virtual method TableEditor::OnSubEditorsLoad");
 

commit 3ec7cd9886182973f688afd893283edb3b6da922

ecere/gui/ColorDropBox: Fixed missing color - Fixed problem introduced by 0faaa3a99957395bcb1a323bf18c8f0e1ffca642

ecere/src/gfx/Color.ec
@@ -36,13 +36,13 @@ public:
       return dropBox.Save();
    }
 
-   Window OnEdit(Window listBox, Window master, int x, int y, int w, int h, Window control)
+   Window OnEdit(Window dataBox, Window master, int x, int y, int w, int h, Window control)
    {
       Size size = { 0 };
       char * string = "";
       ColorDropBox colorDropBox 
       {
-         listBox, master = master, editText = true, //position = Point { x + 24, y }, /*clientSize.h = */size.h = h, size.w = w - 24,
+         dataBox, master = master, editText = true, //position = Point { x + 24, y }, /*clientSize.h = */size.h = h, size.w = w - 24,
          anchor = { left = 24, top = 0, right = 0, bottom = 0 },
          color = this, control = control
       };

ecere/src/gui/controls/DataBox.ec
@@ -136,7 +136,7 @@ private:
 
    void OnRedraw(Surface surface)
    {
-      if(type && (!editor || !editor.created))
+      if(type && (!editor || !editor.created || editor.anchor.left)) // ColorDropBox lets part of the DataBox show
       {
          char tempString[1024];
          if(type._vTbl[__ecereVMethodID_class_OnDisplay] == class(Instance)._vTbl[__ecereVMethodID_class_OnDisplay])