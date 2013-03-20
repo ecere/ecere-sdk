ecere/ecere-sdk

commit 308c3f8a022f58c179f5d52f87be2609fba4ddd8

ecere/gui/EditBox; utf8: Updated EditBox UTF8 iteration; Fixed UTF8GetChar to return 0 in numBytes on null character

ecere/src/com/instance.ec
@@ -6641,7 +6641,7 @@ public unichar UTF8GetChar(char * string, int * numBytes)
    byte b = ((byte *)string)[0];
    int i;
    byte mask = 0x7F;
-   int nb = 1;
+   int nb = b ? 1 : 0;
    ch = 0;
    if(b & 0x80)
    {

ecere/src/gui/controls/EditBox.ec
@@ -47,28 +47,48 @@ public:
       byte b = (string)[0]; \
       int i; \
       byte mask = 0x7F; \
-      numBytes = 1; \
+      numBytes = b ? 1 : 0; \
       ch = 0; \
-      if(b & 0x80 && b & 0x40) \
+      if(b & 0x80) \
       { \
-         mask >>= 2; \
-         numBytes++; \
-         if(b & 0x20) \
+         if(b & 0x40) \
          { \
+            mask >>= 2; \
             numBytes++; \
-            mask >>= 1; \
-            if(b & 0x10) \
+            if(b & 0x20) \
             { \
                numBytes++; \
                mask >>= 1; \
+               if(b & 0x10) \
+               { \
+                  if(b & 0x08) { numBytes = 0; } \
+                  numBytes++; \
+                  mask >>= 1; \
+               } \
             } \
          } \
+         else \
+            numBytes = 0; \
       } \
       for(i = 0; i<numBytes; i++) \
       { \
          ch <<= 6; \
-         ch |= (string)[i] & mask; \
+         ch |= (b = (string)[i]) & mask; \
          mask = 0x3F; \
+         if(i > 1 && (!(b & 0x80) || (b & 0x40))) \
+         { \
+            numBytes = 0; \
+            ch = 0; \
+         } \
+      } \
+      if(i < numBytes || \
+         ch > 0x10FFFF || (ch >= 0xD800 && ch <= 0xDFFF) || \
+        (ch < 0x80 && numBytes > 1) || \
+        (ch < 0x800 && numBytes > 2) || \
+        (ch < 0x10000 && numBytes > 3))\
+      { \
+         ch = 0; \
+         numBytes = 0; \
       } \
       ch; \
    })

compiler/bootstrap/ecere/bootstrap/instance.c
@@ -5739,7 +5739,7 @@ unsigned int ch;
 unsigned char b = ((unsigned char *)string)[0];
 int i;
 unsigned char mask = (unsigned char)0x7F;
-int nb = 1;
+int nb = b ? 1 : 0;
 
 ch = 0;
 if(b & (unsigned char)0x80)

commit 9061adc1be6515da8132aec7e124b14236e148b8

ecere/gui/EditBox: Fixed hanging caused by 308c3f8a022f58c179f5d52f87be2609fba4ddd8 - The EditBox assumed the UTF8_GET_CHAR macro would return numBytes = 1 for \0   (But e.g. the text rendering in LFBDisplayDriver.ec assumed numBytes would be 0)

ecere/locale/ecere.pot
@@ -185,7 +185,7 @@ msgid "Control contains other controls. Delete control and children?"
 msgstr "Control contains other controls. Delete control and children?"
 
 #: ./src/gui/controls/EditBox.ec:966
-#: ./src/gui/controls/EditBox.ec:3086
+#: ./src/gui/controls/EditBox.ec:3085
 msgid "Copy\tCtrl+C"
 msgstr "Copy\tCtrl+C"
 
@@ -215,7 +215,7 @@ msgid "Custom"
 msgstr "Custom"
 
 #: ./src/gui/controls/EditBox.ec:955
-#: ./src/gui/controls/EditBox.ec:3085
+#: ./src/gui/controls/EditBox.ec:3084
 msgid "Cut\tCtrl+X"
 msgstr "Cut\tCtrl+X"
 
@@ -240,7 +240,7 @@ msgid "Defined"
 msgstr "Defined"
 
 #: ./src/gui/controls/EditBox.ec:987
-#: ./src/gui/controls/EditBox.ec:3088
+#: ./src/gui/controls/EditBox.ec:3087
 msgid "Delete\tDel"
 msgstr "Delete\tDel"
 
@@ -579,7 +579,7 @@ msgid "Open"
 msgstr "Open"
 
 #: ./src/gui/controls/EditBox.ec:976
-#: ./src/gui/controls/EditBox.ec:3087
+#: ./src/gui/controls/EditBox.ec:3086
 msgid "Paste\tCtrl+V"
 msgstr "Paste\tCtrl+V"
 
@@ -686,7 +686,7 @@ msgid "Select"
 msgstr "Select"
 
 #: ./src/gui/controls/EditBox.ec:999
-#: ./src/gui/controls/EditBox.ec:3090
+#: ./src/gui/controls/EditBox.ec:3089
 msgid "Select All\tCtrl+A"
 msgstr "Select All\tCtrl+A"
 

ecere/src/com/instance.ec
@@ -6635,6 +6635,7 @@ public int UTF16toUTF8Buffer(uint16 * source, byte * dest, int max)
    return d;
 }
 
+// NOTE: UTF8GetChar now returns 0 into numBytes for the null-terminating character ('\0')
 public unichar UTF8GetChar(char * string, int * numBytes)
 {
    unichar ch;

ecere/src/gui/controls/EditBox.ec
@@ -2024,9 +2024,8 @@ private:
       int c, position = 0;
       unichar ch;
       int nb;
-      for(c = 0; c<this.line.count && c<this.x; c+= nb)
+      for(c = 0; c<this.line.count && c<this.x && (ch = UTF8_GET_CHAR(this.line.buffer + c, nb)); c+= nb)
       {
-         ch = UTF8_GET_CHAR(this.line.buffer + c, nb);
          // TODO: MIGHT WANT TO RETHINK WHAT COLUMN SHOULD BE REGARDING TABS
          if(ch == '\t')
             position += this.tabSize - (position % this.tabSize);
@@ -3369,7 +3368,7 @@ private:
             for(c = start; c<line.count; c += numBytes)
             {
                unichar ch = UTF8_GET_CHAR(line.buffer + c, numBytes);
-               if(!IS_ALUNDER(ch))
+               if(!ch || !IS_ALUNDER(ch))
                   break;
             }
             SelDirty();
@@ -3811,9 +3810,9 @@ private:
                      int start = (line == this.line) ? this.x : 0;
                      int c;
                      int numBytes;
-                     for(c = start; c < line.count; c += numBytes)
+                     unichar ch;
+                     for(c = start; c < line.count && (ch = UTF8_GET_CHAR(line.buffer + c, numBytes)); c += numBytes)
                      {
-                        unichar ch = UTF8_GET_CHAR(line.buffer + c, numBytes);
                         if(IS_ALUNDER(ch))
                         {
                            foundAlpha = true;
@@ -3860,9 +3859,9 @@ private:
                      int start = (line == this.line) ? this.x : 0;
                      int c;
                      int numBytes;
-                     for(c = start; c < line.count; c += numBytes)
+                     unichar ch;
+                     for(c = start; c < line.count && (ch = UTF8_GET_CHAR(line.buffer + c, numBytes)); c += numBytes)
                      {
-                        unichar ch = UTF8_GET_CHAR(line.buffer + c, numBytes);
                         if(!IS_ALUNDER(ch))
                            foundAlpha = true;
                         else if(foundAlpha)