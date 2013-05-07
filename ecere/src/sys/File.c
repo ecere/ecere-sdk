545934b9b3b8ba853998fb97e0e4ebf6d55c51aa
Rejean Loyer <rejean.loyer@gmail.com>
2013-05-07 09:06:35 -0400

    ide; fixed project settings additional include dirs cant add current (./) dir. also fixed trim spaces around paths in compiler settings and project settings and handling unwanted chars. closes #522 and #809.

--- ecere/src/sys/File.c
+++ ecere/src/sys/File.c
@@ -48,6 +48,7 @@ void * __ecereNameSpace__ecere__com__eSystem_Renew0(void * memory, unsigned int 
 unsigned short * __ecereNameSpace__ecere__sys__UTF8toUTF16(char * source, int * wordCount);
 unsigned short * __ecereNameSpace__ecere__sys__UTF8toUTF16Buffer(char * source, uint16 * dest, int max);
 char * __ecereNameSpace__ecere__sys__UTF16toUTF8(uint16 * source);
+void __ecereNameSpace__ecere__sys__ChangeCh(char * string, char ch1, char ch2);
 
 #if defined(__WIN32__) || defined(__WATCOMC__)
 #include <direct.h>
@@ -545,6 +546,8 @@ void FILE_FileFixCase(char * file)
       }
       strcpy(file, parent);
    }
+#else
+   __ecereNameSpace__ecere__sys__ChangeCh(file, '\\', '/');
 #endif
 }
 
