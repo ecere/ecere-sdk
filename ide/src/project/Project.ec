ecere/ecere-sdk

commit 9b2b296033c3b987d6a63d44ba01ae340d824200

ide; ear/extract: (#937) Not calling UPX for 64 bit Windows executables (not supported)

ide/src/project/Project.ec
@@ -3266,7 +3266,9 @@ private:
                   f.Puts("\t$(UPX) $(UPXFLAGS) $(TARGET)\n");
                f.Puts("endif\n");
                f.Puts("else\n");
+               f.Puts("ifneq \"$(TARGET_ARCH)\" \"x86_64\"");
                   f.Puts("\t$(UPX) $(UPXFLAGS) $(TARGET)\n");
+               f.Puts("endif\n");
                f.Puts("endif\n");
             }
          }

ear/extract/Makefile
@@ -201,7 +201,9 @@ ifdef EXECUTABLE_TARGET
 	@-$(call sys_path,$(UPX) $(UPXFLAGS) $(TARGET)) || $(call echo,upx not installed; not compressing.)
 endif
 else
+ifneq "$(TARGET_ARCH)" "x86_64"
 	@-$(call sys_path,$(UPX) $(UPXFLAGS) $(TARGET)) || $(call echo,upx not installed; not compressing.)
+endif
 endif
 	$(EAR) aw$(EARFLAGS) $(TARGET) ../../ecere/res/vanilla/ecere/actions/folderNew.png ../../ecere/res/vanilla/ecere/actions/goUp.png "ecere/actions"
 	$(EAR) aw$(EARFLAGS) $(TARGET) ../../ecere/res/vanilla/ecere/devices/computer.png ../../ecere/res/vanilla/ecere/devices/driveHardDisk.png ../../ecere/res/vanilla/ecere/devices/driveRemovableMedia.png ../../ecere/res/vanilla/ecere/devices/mediaFloppy.png ../../ecere/res/vanilla/ecere/devices/mediaOptical.png "ecere/devices"

commit 252da215cc039fa1c78b025846a390ebf28de0c9

ide/Project: Fixed broken UPX compression Makefile introduced by 9b2b296033c3b987d6a63d44ba01ae340d824200

ide/src/project/Project.ec
@@ -3282,7 +3282,7 @@ private:
                   f.Puts("\t$(UPX) $(UPXFLAGS) $(TARGET)\n");
                f.Puts("endif\n");
                f.Puts("else\n");
-               f.Puts("ifneq \"$(TARGET_ARCH)\" \"x86_64\"");
+               f.Puts("ifneq \"$(TARGET_ARCH)\" \"x86_64\"\n");
                   f.Puts("\t$(UPX) $(UPXFLAGS) $(TARGET)\n");
                f.Puts("endif\n");
                f.Puts("endif\n");