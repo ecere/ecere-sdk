ecere/ecere-sdk

commit d0d99da885cb22b451821053009c5af5abe39be0

compiler/libec; ecere:SecSince1970: (#1072) Generating an error rather than invalid C code for unit class property set

compiler/libec/src/pass0.ec
@@ -1200,15 +1200,14 @@ static void ProcessClass(ClassType classType, OldList definitions, Symbol symbol
                         MkDeclaratorIdentifier(MkIdentifier(name)), params);
 
                      // TESTING COMMENTING OUT THESE, ADDED noHeadClass here
-                     if(!propertyDef.symbol._property || !propertyDef.symbol._property.conversion)
+                     if((regClass.type == structClass || regClass.type == unionClass) ||
+                        ((regClass.type == noHeadClass || regClass.type == normalClass) && (!propertyDef.symbol._property || !propertyDef.symbol._property.conversion)))
                         ListAdd(specifiers, MkSpecifier(VOID));
                      else
                      {
-                        // Conversion property
-                        if(regClass.type == structClass)
-                           ListAdd(specifiers, MkSpecifier(VOID));
-                        else
-                           ListAdd(specifiers, MkSpecifierName(regClass.fullName));
+                        if(regClass.type != noHeadClass && regClass.type != normalClass)
+                           Compiler_Error($"set defined on type without storage for non-conversion property\n");
+                        ListAdd(specifiers, MkSpecifierName(regClass.fullName));
                      }
 
                      func = MkClassFunction(specifiers, null, decl, null);

compiler/bootstrap/libec/bootstrap/pass0.c
@@ -2240,13 +2240,12 @@ MangleClassName(name);
 params = MkList();
 ListAdd(params, MkTypeName(CopyList(propertyDef->specifiers, CopySpecifier), PlugDeclarator(propertyDef->declarator, MkDeclaratorIdentifier(MkIdentifier("value")))));
 decl = MkDeclaratorFunction(MkDeclaratorIdentifier(MkIdentifier(name)), params);
-if(!propertyDef->symbol->_property || !propertyDef->symbol->_property->conversion)
+if((regClass->type == 1 || regClass->type == 6) || ((regClass->type == 5 || regClass->type == 0) && (!propertyDef->symbol->_property || !propertyDef->symbol->_property->conversion)))
 ListAdd(specifiers, MkSpecifier(VOID));
 else
 {
-if(regClass->type == 1)
-ListAdd(specifiers, MkSpecifier(VOID));
-else
+if(regClass->type != 5 && regClass->type != 0)
+Compiler_Error(__ecereNameSpace__ecere__GetTranslatedString("ec", "set defined on type without storage for non-conversion property\n", (((void *)0))));
 ListAdd(specifiers, MkSpecifierName(regClass->fullName));
 }
 func = MkClassFunction(specifiers, (((void *)0)), decl, (((void *)0)));

compiler/libec/src/ast.ec
@@ -1147,7 +1147,7 @@ FunctionDefinition _MkFunction(OldList specifiers, Declarator declarator, OldLis
                   Specifier spec = tn.qualifiers ? tn.qualifiers->first : null;
                   if(!tn.declarator && !tn.prev && !tn.next && spec && !spec.next && spec.type == baseSpecifier && spec.specifier == VOID);
                   else
-                     Compiler_Error("parameter name omitted\n");
+                     Compiler_Error($"parameter name omitted\n");
                   break;
                }
             }

ecere/src/sys/Time.ec
@@ -316,8 +316,6 @@ public class SecSince1970 : int64
 public:
    property SecSince1970 global
    {
-      // TOFIX: 'return' with a value, in function returning void
-      set { return value.local; }
       get
       {
       #if defined(__WIN32__)
@@ -371,8 +369,6 @@ public:
    };
    property SecSince1970 local
    {
-      // TOFIX: warning: 'return' with a value, in function returning void
-      set { return value.global; }
       get
       {
 #if defined(__WIN32__)

commit 1bb2c028ff1c6e8abb073ed7a69aaf4054b01971

compiler/libec: Fixed DB active records broken by d0d99da885cb22b451821053009c5af5abe39be0 - Property sets on Row ID classes were being used to set values in the database   An exception is now made for those classes not to generate this error

compiler/libec/precompiled/expression.c
@@ -853,9 +853,13 @@ struct Statement * getStmt;
 struct Statement * setStmt;
 struct Statement * issetStmt;
 struct Symbol * symbol;
-unsigned int conversion;
-unsigned int isWatchable;
 struct Expression * category;
+struct
+{
+unsigned int conversion : 1;
+unsigned int isWatchable : 1;
+unsigned int isDBProp : 1;
+} __attribute__ ((gcc_struct));
 } __attribute__ ((gcc_struct));
 
 extern struct __ecereNameSpace__ecere__com__Class * __ecereClass_PropertyWatch;

compiler/libec/precompiled/grammar.c
@@ -989,9 +989,13 @@ struct Statement * getStmt;
 struct Statement * setStmt;
 struct Statement * issetStmt;
 struct Symbol * symbol;
-unsigned int conversion;
-unsigned int isWatchable;
 struct Expression * category;
+struct
+{
+unsigned int conversion : 1;
+unsigned int isWatchable : 1;
+unsigned int isDBProp : 1;
+} __attribute__ ((gcc_struct));
 } __attribute__ ((gcc_struct));
 
 extern struct __ecereNameSpace__ecere__com__Class * __ecereClass_PropertyWatch;

compiler/libec/precompiled/type.c
@@ -879,9 +879,13 @@ struct Statement * getStmt;
 struct Statement * setStmt;
 struct Statement * issetStmt;
 struct Symbol * symbol;
-unsigned int conversion;
-unsigned int isWatchable;
 struct Expression * category;
+struct
+{
+unsigned int conversion : 1;
+unsigned int isWatchable : 1;
+unsigned int isDBProp : 1;
+} __attribute__ ((gcc_struct));
 } __attribute__ ((gcc_struct));
 
 extern struct __ecereNameSpace__ecere__com__Class * __ecereClass_PropertyWatch;

compiler/libec/src/dbpass.ec
@@ -818,6 +818,7 @@ static void ProcessDBTable(DBTableDef table)
                         CopyList(entry.dataType.qualifiers, CopySpecifier), CopyDeclarator(entry.dataType.declarator),
                            MkIdentifier(name), rowSet, null));
                      def.propertyDef.symbol.id = def.propertyDef.symbol.idCode = symbolID;
+                     def.propertyDef.isDBProp = true;
                      def.memberAccess = publicAccess;
                      rowClassDefs->Add(def);
                   }
@@ -873,6 +874,7 @@ static void ProcessDBTable(DBTableDef table)
                   curContext = globalContext;
                   def = MkClassDefProperty(MkProperty(CopyList(entry.dataType.qualifiers, CopySpecifier), entry.dataType.declarator, CopyIdentifier(entry.id), rowSet, rowGet));
                   def.propertyDef.symbol.id = def.propertyDef.symbol.idCode = symbolID;
+                  def.propertyDef.isDBProp = true;
                   def.memberAccess = publicAccess;
                   rowClassDefs->Add(def);
                }
@@ -1044,6 +1046,7 @@ static void ProcessDBTable(DBTableDef table)
                      CopyList(entry.dataType.qualifiers, CopySpecifier), CopyDeclarator(entry.dataType.declarator),
                         CopyIdentifier(entry.id), idSet, idGet));
                   def.propertyDef.symbol.id = def.propertyDef.symbol.idCode = symbolID;
+                  def.propertyDef.isDBProp = true;
                   def.memberAccess = publicAccess;
                   idClassDefs->Add(def);
                }

compiler/libec/src/ecdefs.ec
@@ -923,9 +923,13 @@ public:
    Statement setStmt;
    Statement issetStmt;
    Symbol symbol;
-   bool conversion;
-   bool isWatchable;
    Expression category;
+   struct
+   {
+      bool conversion:1;
+      bool isWatchable:1;
+      bool isDBProp:1;
+   };
 };
 
 public class ClassDef : struct

compiler/libec/src/pass0.ec
@@ -1205,7 +1205,7 @@ static void ProcessClass(ClassType classType, OldList definitions, Symbol symbol
                         ListAdd(specifiers, MkSpecifier(VOID));
                      else
                      {
-                        if(regClass.type != noHeadClass && regClass.type != normalClass && (!propertyDef.symbol._property || !propertyDef.symbol._property.conversion))
+                        if(regClass.type != noHeadClass && regClass.type != normalClass && !propertyDef.isDBProp && (!propertyDef.symbol._property || !propertyDef.symbol._property.conversion))
                            Compiler_Error($"set defined on type without storage for non-conversion property\n");
                         ListAdd(specifiers, MkSpecifierName(regClass.fullName));
                      }