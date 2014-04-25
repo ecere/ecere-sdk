ecere/ecere-sdk

commit e32618e4e2849f3fe6c74c172ff84976de218542

ecere/gfx/Bitmap: Fixed crash on reloading textures from X11 to GL

ecere/src/gfx/Bitmap.ec
@@ -860,7 +860,9 @@ public:
    {
       if(this && driver)
       {
-         driver.FreeBitmap(displaySystem, this);
+         if(driverData)
+            driver.FreeBitmap(displaySystem, this);
+         driverData = null;
       }
       if(this && keepData)
          delete picture;

commit bf1402c3ffc0121cdf01951b2fe2b6be5e76d729

ecere/gfx/Bitmap; drivers: Fixed leaks introduced by e32618e4e2849f3fe6c74c172ff84976de218542

ecere/src/gfx/Bitmap.ec
@@ -860,8 +860,7 @@ public:
    {
       if(this && driver)
       {
-         if(driverData)
-            driver.FreeBitmap(displaySystem, this);
+         driver.FreeBitmap(displaySystem, this);
          driverData = null;
       }
       if(this && keepData)

ecere/src/gfx/drivers/OpenGLDisplayDriver.ec
@@ -2251,9 +2251,11 @@ class OpenGLDisplayDriver : DisplayDriver
 
    void FreeBitmap(DisplaySystem displaySystem, Bitmap bitmap)
    {
-      glDeleteTextures(1, (int *)&bitmap.driverData);
-      bitmap.driverData = 0;
-
+      if(bitmap.driverData)
+      {
+         glDeleteTextures(1, (int *)&bitmap.driverData);
+         bitmap.driverData = 0;
+      }
       bitmap.driver = ((subclass(DisplayDriver))class(LFBDisplayDriver));
    }
 

ecere/src/gfx/drivers/XDisplayDriver.ec
@@ -634,16 +634,19 @@ class XDisplayDriver : DisplayDriver
    void FreeBitmap(DisplaySystem displaySystem, Bitmap bitmap)
    {
       XBitmap xBitmap = bitmap.driverData;
-
-      if(xBitmap.pixmap)
-         XFreePixmap(xGlobalDisplay, xBitmap.pixmap);
-      if(xBitmap.mask)
-         XFreePixmap(xGlobalDisplay, xBitmap.mask);
-      if(xBitmap.picture)
-         XRenderFreePicture(xGlobalDisplay, xBitmap.picture);
-      if(xBitmap.maskPicture)
-         XRenderFreePicture(xGlobalDisplay, xBitmap.maskPicture);
-      delete xBitmap;
+      if(xBitmap)
+      {
+         if(xBitmap.pixmap)
+            XFreePixmap(xGlobalDisplay, xBitmap.pixmap);
+         if(xBitmap.mask)
+            XFreePixmap(xGlobalDisplay, xBitmap.mask);
+         if(xBitmap.picture)
+            XRenderFreePicture(xGlobalDisplay, xBitmap.picture);
+         if(xBitmap.maskPicture)
+            XRenderFreePicture(xGlobalDisplay, xBitmap.maskPicture);
+         delete xBitmap;
+         bitmap.driverData = null;
+      }
       ((subclass(DisplayDriver))class(LFBDisplayDriver)).FreeBitmap(displaySystem, bitmap);
    }
 