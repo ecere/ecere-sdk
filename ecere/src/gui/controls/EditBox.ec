ecere/ecere-sdk

commit 22b2adb1b99fa6ef40e38b9c31575c41eca3878a

ecere/gui/EditBox:Syntax Highlighting: Optimization to solve hangs on long lines - This was making grammar.c completely unresponsive - This might also fix potential issues with p/P and e/E suffixes

ecere/src/gui/controls/EditBox.ec
@@ -11,6 +11,17 @@ import "FindDialog"
 import "GoToDialog"
 import "Array"
 
+char * strchrmax(const char * s, int c, int max)
+{
+   char * result = null;
+   int i;
+   char ch;
+   for(i = 0; i < max && (ch = s[i]); i++)
+      if(ch == c)
+         return s + i;
+   return null;
+}
+
 public class SyntaxColorScheme
 {
 public:
@@ -1775,25 +1786,25 @@ private:
                            if(!wasEscaped)
                               escaped = true;
                         }
-                        else if(!inQuotes && !inString && !inMultiLineComment && !inSingleLineComment && (isdigit(word[0]) || (word[0] == '.' && isdigit(word[1]))))
+                        else if(x < box.right && !inQuotes && !inString && !inMultiLineComment && !inSingleLineComment && (isdigit(word[0]) || (word[0] == '.' && isdigit(word[1]))))
                         {
-                           char * dot = strchr(word, '.');
+                           char * dot = strchrmax(word, '.', wordLen);
                            bool isHex = (word[0] == '0' && (word[1] == 'x' || word[1] == 'X'));
                            char * exponent;
                            bool isReal;
                            char * s = null;
                            if(isHex)
                            {
-                              exponent = strchr(word, 'p');
-                              if(!exponent) exponent = strchr(word, 'P');
+                              exponent = strchrmax(word, 'p', wordLen);
+                              if(!exponent) exponent = strchrmax(word, 'P', wordLen);
                            }
                            else
                            {
-                              exponent = strchr(word, 'e');
-                              if(!exponent) exponent = strchr(word, 'E');
+                              exponent = strchrmax(word, 'e', wordLen);
+                              if(!exponent) exponent = strchrmax(word, 'E', wordLen);
                            }
-                           if(exponent && exponent > word + wordLen) exponent = null;
-                           if(dot && dot > word + wordLen) dot = null;
+                           //if(exponent && exponent > word + wordLen) exponent = null;
+                           //if(dot && dot > word + wordLen) dot = null;
                            isReal = dot || exponent;
                            if(isReal)
                               strtod(word, &s);      // strtod() seems to break on hex floats (e.g. 0x23e3p12, 0x1.fp3)
@@ -1859,7 +1870,7 @@ private:
                                  newTextColor = colorScheme.preprocessorColor;
                               }
                            }
-                           if(!inQuotes && !inString && !inMultiLineComment && !inSingleLineComment)
+                           if(x < box.right && !inQuotes && !inString && !inMultiLineComment && !inSingleLineComment)
                            {
                               for(g = 0; g < ((inPrep && word[0] != '#') ? 2 : 1); g++)
                               {

commit cd586afcf9a1d41b5c7af4e63b1b214471b5f9e2

ecere/gui/EditBox:Highligting: (#1034, #1073) Fixed syntax highlighting of real values broken by 22b2adb1b99fa6ef40e38b9c31575c41eca3878a - Text after the . was not included in wordLen, simplified the whole thing based on the fact that   exponent is only used to determine whether to call strtod or not

ecere/src/gui/controls/EditBox.ec
@@ -1788,24 +1788,27 @@ private:
                         }
                         else if(x < box.right && !inQuotes && !inString && !inMultiLineComment && !inSingleLineComment && (isdigit(word[0]) || (word[0] == '.' && isdigit(word[1]))))
                         {
-                           char * dot = strchrmax(word, '.', wordLen);
-                           bool isHex = (word[0] == '0' && (word[1] == 'x' || word[1] == 'X'));
-                           char * exponent;
-                           bool isReal;
+                           char * dot = word[wordLen] == '.' ? word + wordLen : null;
+                           bool isReal = dot != null;
                            char * s = null;
-                           if(isHex)
-                           {
-                              exponent = strchrmax(word, 'p', wordLen);
-                              if(!exponent) exponent = strchrmax(word, 'P', wordLen);
-                           }
+                           if(dot)
+                              isReal = true;
                            else
                            {
-                              exponent = strchrmax(word, 'e', wordLen);
-                              if(!exponent) exponent = strchrmax(word, 'E', wordLen);
+                              char * exponent;
+                              bool isHex = (word[0] == '0' && (word[1] == 'x' || word[1] == 'X'));
+                              if(isHex)
+                              {
+                                 exponent = strchrmax(word, 'p', wordLen);
+                                 if(!exponent) exponent = strchrmax(word, 'P', wordLen);
+                              }
+                              else
+                              {
+                                 exponent = strchrmax(word, 'e', wordLen);
+                                 if(!exponent) exponent = strchrmax(word, 'E', wordLen);
+                              }
+                              isReal = exponent != null;
                            }
-                           //if(exponent && exponent > word + wordLen) exponent = null;
-                           //if(dot && dot > word + wordLen) dot = null;
-                           isReal = dot || exponent;
                            if(isReal)
                               strtod(word, &s);      // strtod() seems to break on hex floats (e.g. 0x23e3p12, 0x1.fp3)
                            else