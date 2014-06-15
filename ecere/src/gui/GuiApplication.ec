ecere/ecere-sdk

commit ab4dde3bdaf6541408ad30b6252c63ca4af5fa80

ecere/gui/Window: Avoiding a call to OnMouseLeave() when over a child window

ecere/src/gui/Window.ec
@@ -4253,7 +4253,7 @@ private:
                guiApp.prevWindow = null;
 
                // Eventually fix this not to include captured?
-               if(!prevWindow.OnMouseLeave(*mods))
+               if(!trueWindow.IsDescendantOf(prevWindow) && !prevWindow.OnMouseLeave(*mods))
                   result = false;
             }
             if(result && trueWindow && !trueWindow.destroyed/* && trueWindow == window*/)

commit f5dce423a4a37aad19f2233c25836ed55d43b9b3

ecere/gui/Window: (#1083) Fixed stuck tooltips, stuck/crashing IDE - Problem was introduced with ab4dde3bdaf6541408ad30b6252c63ca4af5fa80, not calling OnMouseLeave while still over child windows - Now keeping a list of ancestors to call OnMouseLeave for

ecere/src/gui/GuiApplication.ec
@@ -82,6 +82,7 @@ import "XInterface"
 #endif
 
 import "Window"
+import "List"
 
 /*static */bool guiApplicationInitialized = false;
 GuiApplication guiApp;
@@ -123,7 +124,8 @@ public class GuiApplication : Application
    OldList windowTimers;
 
    // Mouse events
-   Window prevWindow;     // Used for OnMouseLeave
+   Window prevWindow;            // Used for OnMouseLeave
+   List<Window> overWindows { }; // Used for OnMouseLeave
    Window windowCaptured;
 
    // Mouse based moving & resizing

ecere/src/gui/Window.ec
@@ -4269,14 +4269,37 @@ private:
          if(guiApp.windowCaptured || trueWindow)
          {
             Window prevWindow = guiApp.prevWindow;
-            if(guiApp.prevWindow && trueWindow != guiApp.prevWindow)
+            List<Window> overWindows = guiApp.overWindows;
+            Iterator<Window> it { overWindows };
+
+            while(it.Next())
+            {
+               Window w = it.data;
+               if(trueWindow != w && !trueWindow.IsDescendantOf(w))
+               {
+                  it.pointer = null;
+                  result = w.OnMouseLeave(*mods);
+                  if(!result) break;
+                  overWindows.TakeOut(w);
+               }
+            }
+
+            if(result && guiApp.prevWindow && trueWindow != guiApp.prevWindow)
             {
                guiApp.prevWindow.mouseInside = false;
                guiApp.prevWindow = null;
 
-               // Eventually fix this not to include captured?
-               if(!trueWindow.IsDescendantOf(prevWindow) && !prevWindow.OnMouseLeave(*mods))
-                  result = false;
+               if(result)
+               {
+                  if(trueWindow.IsDescendantOf(prevWindow))
+                  {
+                     if(!overWindows.Find(prevWindow))
+                        overWindows.Add(prevWindow);
+                  }
+                  // Eventually fix this not to include captured?
+                  else if(!prevWindow.OnMouseLeave(*mods))
+                     result = false;
+               }
             }
             if(result && trueWindow && !trueWindow.destroyed/* && trueWindow == window*/)
             {