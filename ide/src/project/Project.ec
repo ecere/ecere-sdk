ecere/ecere-sdk

commit eff6d0cfcbf71a54870f8a965174bb641b24fcb1

ecere; ide: Fixed Release config warnings

ide/src/project/Project.ec
@@ -1586,7 +1586,7 @@ private:
                   else if(strstr(test, strip) == test);
                   else if(strstr(test, cc) == test || strstr(test, cxx) == test || strstr(test, ecp) == test || strstr(test, ecc) == test)
                   {
-                     char * module;
+                     char * module = null;
                      bool isPrecomp = false;
                      bool gotCC = false;
 
@@ -4029,7 +4029,7 @@ Project LegacyAsciiLoadProject(File f, const char * filePath)
    char parentPath[MAX_LOCATION];
    char section[128] = "";
    char subSection[128] = "";
-   ProjectNode parent;
+   ProjectNode parent = project.topNode;
    bool configurationsPresent = false;
 
    f.Seek(0, start);

compiler/libec/ec.epj
@@ -37,7 +37,7 @@
       {
          "Name" : "Release",
          "Options" : {
-            "Warnings" : "None",
+            "Warnings" : "All",
             "Debug" : false,
             "NoLineNumbers" : true,
             "Optimization" : "Speed",

ecere/ecere.epj
@@ -245,7 +245,7 @@ from wherever you obtained them.
       {
          "Name" : "Release",
          "Options" : {
-            "Warnings" : "None",
+            "Warnings" : "All",
             "NoLineNumbers" : true,
             "Optimization" : "Speed",
             "FastMath" : true,

ecere/src/com/String.ec
@@ -198,7 +198,7 @@ public char * PathCatSlash(char * string, const char * addedPath)
       int c = 0;
       bool isURL = false;
       bool isArchive = SplitArchivePath(string, archiveName, &file);
-      char * urlFileName;
+      char * urlFileName = null;
       char * protocolSymbol;
 
       strcpy(fileName, isArchive ? file : string);
@@ -418,7 +418,7 @@ public char * PathCat(char * string, const char * addedPath)
       int c = 0;
       bool isURL = false;
       bool isArchive = SplitArchivePath(string, archiveName, &file);
-      char * urlFileName;
+      char * urlFileName = null;
       char * protocolSymbol;
 
       strcpy(fileName, isArchive ? file : string);

ecere/src/com/dataTypes.ec
@@ -197,7 +197,7 @@ public:
 {
    NamedLink64 item = null;
    Class b;
-   int64 i64Data;
+   int64 i64Data = 0;
    switch(_class.typeSize)
    {
       case 1:

ecere/src/com/instance.c
@@ -415,7 +415,8 @@ void Instance_COM_Initialize(int argc, char ** argv, char ** parsedCommand, int 
 #if defined(__unix__)
    if(!__thisModule && argv)
    {
-      getcwd(exeLocation, MAX_LOCATION);
+      if(!getcwd(exeLocation, MAX_LOCATION))
+         exeLocation[0] = 0;
       PathCat(exeLocation, argv[0]);
    }
 #endif

ecere/src/gfx/Color.ec
@@ -148,7 +148,7 @@ public struct ColorHSV
          float r = value.r, g = value.g, b = value.b;
          float minV, maxV, diff,dr,dg,db;
 
-         float h, s, v;
+         float h = 0, s, v;
 
          minV = Min(r, g);
          minV = Min(minV, b);
@@ -209,7 +209,7 @@ public struct ColorHSV
             case 2: r = p; g = v; b = t; break;
             case 3: r = p; g = q; b = v; break;
             case 4: r = t; g = p; b = v; break;
-            case 5: r = v; g = p; b = q; break;
+            case 5: default: r = v; g = p; b = q; break;
          }
          // TODO: Figure out where this is needed
          r = Max(Min(r, 1),0);

ecere/src/gfx/Surface.ec
@@ -19,7 +19,7 @@ public void PaletteGradient(ColorAlpha * palette, int numColors, ColorKey * keys
    float inc = 1.0f/(numColors-1);
    float percent = 0;
    int start;
-   ColorAlpha color;
+   ColorAlpha color = 0;
    int c;
 
    for(c = start = 0; c<numColors; c++)

commit 5018e11f8f287f975d7a903eddeaa5b37eb4c2eb

ide/Project: Fixed a crash in LegacyLoad introduced by eff6d0cfcbf71a54870f8a965174bb641b24fcb1

ide/src/project/Project.ec
@@ -4029,12 +4029,11 @@ void ProjectConfig::LegacyProjectConfigLoad(File f)
 Project LegacyAsciiLoadProject(File f, const char * filePath)
 {
    Project project = null;
-   //ProjectNode node = null;
    int pos;
    char parentPath[MAX_LOCATION];
    char section[128] = "";
    char subSection[128] = "";
-   ProjectNode parent = project.topNode;
+   ProjectNode parent = null;
    bool configurationsPresent = false;
 
    f.Seek(0, start);