ecere/ecere-sdk

commit 2a9a3ae6df44c1a17d65136540cb29386e0f5e97

ecere/gui/Window; drivers/Win32Interface: (#240, #285) Avoid re-creating a new window handle and unmaximizing on changed display settings - #240, #285 might already have been fixed by earlier commits, these were remaining related issues found while testing.

ecere/src/gui/drivers/Win32Interface.ec
@@ -1542,9 +1542,19 @@ class Win32Interface : Interface
          {
             case maximized:
             case normal:
-               ShowWindow(window.windowHandle, ((window.active || window.creationActivation == activate) && !externalDisplayChange) ?
-                  ((window.nativeDecorations && state == maximized) ? SW_MAXIMIZE : SW_SHOWNORMAL) : SW_SHOWNOACTIVATE);
+            {
+               if((window.active || window.creationActivation == activate) && !externalDisplayChange)
+                  ShowWindow(window.windowHandle, (window.nativeDecorations && state == maximized) ? SW_MAXIMIZE : SW_SHOWNORMAL);
+               else
+               {
+                  WINDOWPLACEMENT plc = { 0 };
+                  GetWindowPlacement(window.windowHandle, &plc);
+                  plc.showCmd = (window.nativeDecorations && state == maximized) ? SW_MAXIMIZE : SW_SHOWNORMAL;
+                  ShowWindow(window.windowHandle, SW_SHOWNOACTIVATE);
+                  SetWindowPlacement(window.windowHandle, &plc);
+               }
                break;
+            }
             case minimized:
                ShowWindow(window.windowHandle, SW_MINIMIZE);
                break;

ecere/src/gui/Window.ec
@@ -4832,7 +4832,8 @@ private:
          subclass(DisplayDriver) dDriver = (dispDriver && !formDesigner) ? dispDriver : GetDisplayDriver(guiApp.defaultDisplayDriver);
          DisplaySystem displaySystem = dDriver ? dDriver.displaySystem : null;
 
-         windowHandle = dDriver.printer ? null : guiApp.interfaceDriver.CreateRootWindow(this);
+         if(!windowHandle)
+            windowHandle = dDriver.printer ? null : guiApp.interfaceDriver.CreateRootWindow(this);
 
          // This was here, is it really needed?
          //guiApp.interfaceDriver.ActivateRootWindow(this);

commit ec7476597149e60e272b295eeddf6a9655676b45

ecere/gui/Win32Interface: (#285) Fixed handling lock/fullscreen change - Problem introduced by 2a9a3ae6df44c1a17d65136540cb29386e0f5e97 - Calling both ShowWindow() and SetWindowPlacement() caused a strange effect  sometimes noticed when logging back in from a locked screen or when an application  went fullscreen, happening every time when going fullscreen in a debugged app. - Was mistakingly assuming that WM_MAXIMIZE activates, while only WM_SHOWMAXIMIZED does - Setting 'state' data member from SetRootWindowState() to avoid redundant states being set   again from WM_SIZE thinking that a user initiated the change - Setting initial lastBits and lastRes on startup to avoid processing WM_DISPLAYCHANGE for the   initial settings - Checking whether the foreground window is taking up the fullscreen and not processing   WM_DISPLAYCHANGE when that is the case

ecere/src/gui/drivers/Win32Interface.ec
@@ -112,6 +112,9 @@ static byte key2VK[256] =
 static const uint16 className[] = L"Ecere Application";
 static HINSTANCE hInstance;
 
+static WPARAM lastBits;
+static LPARAM lastRes;
+
 static DEVMODE devMode;
 #ifndef ECERE_NODINPUT
 static HWND acquiredWindow = null;
@@ -175,6 +178,18 @@ static WINDOWPLACEMENT taskBarPlacement;
 static bool activateApp;
 
 static SystemCursor lastCursor = (SystemCursor)-1;
+
+static void SmartShowWindow(HWND windowHandle, WindowState state, bool doActivate)
+{
+   int showCmd;
+   if(state == maximized)
+      showCmd = doActivate ? SW_SHOWMAXIMIZED : SW_MAXIMIZE;
+   else if(state == minimized)
+      showCmd = SW_MINIMIZE;
+   else
+      showCmd = doActivate ? SW_SHOWNORMAL : SW_SHOWNOACTIVATE;
+   ShowWindow(windowHandle, showCmd);
+}
 
 void AeroSnapPosition(Window window, int x, int y, int w, int h)
 {
@@ -611,19 +626,26 @@ class Win32Interface : Interface
             }
             case WM_DISPLAYCHANGE:
             {
-               static int lastBits = 0;
-               static int lastRes = 0;
-               if(lastBits != wParam || lastRes != lParam)
+               if(!guiApp.fullScreenMode && (lastBits != wParam || lastRes != lParam))
                {
-                  lastBits = (int)wParam;
-                  lastRes = (int)lParam;
+                  RECT rect;
+                  HWND foregroundWindow = GetForegroundWindow();
+                  int w = LOWORD(lParam);
+                  int h = HIWORD(lParam);
+
+                  GetWindowRect(foregroundWindow, &rect);
+                  if(rect.right == w && rect.bottom == h)
+                     break;
+
+                  lastBits = wParam;
+                  lastRes = lParam;
 
                   externalDisplayChange = true;
                   if(guiApp.desktop.DisplayModeChanged())
                   {
                      char caption[2048];
                      if(!window.style.hidden)
-                        ShowWindow(windowHandle, SW_SHOWNOACTIVATE /*SW_SHOWNORMAL*/);
+                        SmartShowWindow(window.windowHandle, (window.nativeDecorations && window.state == maximized) ? maximized : normal, false);
                      window.FigureCaption(caption);
                      SetRootWindowCaption(window, caption);
                   }
@@ -1173,6 +1195,11 @@ class Win32Interface : Interface
          null,
          className
       };
+      HDC hdc = GetDC(0);
+      lastRes = MAKELPARAM(GetSystemMetrics(SM_CYSCREEN), GetSystemMetrics(SM_CXSCREEN));
+      lastBits = (WPARAM)GetDeviceCaps(hdc, BITSPIXEL);
+      ReleaseDC(0, hdc);
+
       AttachConsole(-1);
       wcl.hInstance = hInstance = GetModuleHandle(null);
       RegisterClass(&wcl);
@@ -1535,27 +1562,19 @@ class Win32Interface : Interface
    {
       if(visible)
       {
+         WindowState curState = window.state;
+         *&window.state = state;
          switch(state)
          {
             case maximized:
             case normal:
-            {
-               if((window.active || window.creationActivation == activate) && !externalDisplayChange)
-                  ShowWindow(window.windowHandle, (window.nativeDecorations && state == maximized) ? SW_MAXIMIZE : SW_SHOWNORMAL);
-               else
-               {
-                  WINDOWPLACEMENT plc = { 0 };
-                  GetWindowPlacement(window.windowHandle, &plc);
-                  plc.showCmd = (window.nativeDecorations && state == maximized) ? SW_MAXIMIZE : SW_SHOWNORMAL;
-                  ShowWindow(window.windowHandle, SW_SHOWNOACTIVATE);
-                  SetWindowPlacement(window.windowHandle, &plc);
-               }
+               SmartShowWindow(window.windowHandle, window.nativeDecorations ? state : normal, (window.active || window.creationActivation == activate) && !externalDisplayChange);
                break;
-            }
             case minimized:
                ShowWindow(window.windowHandle, SW_MINIMIZE);
                break;
          }
+         *&window.state = curState;
       }
       else
       {