ecere/ecere-sdk

commit fa765a838029ec71a86787e95ebcab5fda0aae1e

compiler/libec: (#307, #70) Warning on undeclared class; Overriding namespaces

compiler/bootstrap/libec/bootstrap/ast.c
@@ -588,46 +588,6 @@ return CheckType(yytext);
 
 struct DBTableEntry;
 
-struct Symbol *  _DeclClass(const char *  name);
-
-struct Symbol * DeclClassAddNameSpace(const char * className)
-{
-char name[1024];
-int len = 0, stringLen;
-
-name[0] = '\0';
-if((currentNameSpace || defaultNameSpace) && declMode != 0 && defaultDeclMode != 0)
-{
-if(defaultNameSpace)
-{
-memcpy(name, defaultNameSpace, defaultNameSpaceLen);
-len += defaultNameSpaceLen;
-name[len++] = ':';
-name[len++] = ':';
-}
-if(currentNameSpace)
-{
-memcpy(name + len, currentNameSpace, currentNameSpaceLen);
-len += currentNameSpaceLen;
-name[len++] = ':';
-name[len++] = ':';
-}
-}
-stringLen = strlen(className);
-memcpy(name + len, className, stringLen);
-len += stringLen;
-name[len] = (char)0;
-return _DeclClass(name);
-}
-
-struct Symbol * DeclClass(const char * name)
-{
-if(strchr(name, ':'))
-return _DeclClass(name);
-else
-return DeclClassAddNameSpace(name);
-}
-
 struct MemberInit;
 
 struct Pointer;
@@ -725,6 +685,7 @@ struct __ecereNameSpace__ecere__sys__OldList templatedClasses;
 struct Context * ctx;
 int isIterator;
 struct Expression * propCategory;
+unsigned int mustRegister;
 } __attribute__ ((gcc_struct));
 
 extern struct __ecereNameSpace__ecere__com__Method * __ecereNameSpace__ecere__com__eClass_AddMethod(struct __ecereNameSpace__ecere__com__Class * _class, const char *  name, const char *  type, void *  function, int declMode);
@@ -1004,6 +965,7 @@ struct ExtDecl * extDecl;
 char *  name;
 struct Symbol * symbol;
 struct __ecereNameSpace__ecere__sys__OldList *  templateArgs;
+struct Specifier * nsSpec;
 } __attribute__ ((gcc_struct)) __anon1;
 struct
 {
@@ -1208,6 +1170,46 @@ struct Location nameLoc;
 struct Location insideLoc;
 unsigned int built;
 } __attribute__ ((gcc_struct));
+
+struct Symbol *  _DeclClass(struct Specifier *  _class, const char *  name);
+
+struct Symbol * DeclClassAddNameSpace(struct Specifier * _class, const char * className)
+{
+char name[1024];
+int len = 0, stringLen;
+
+name[0] = '\0';
+if(className[0] != ':' && (currentNameSpace || defaultNameSpace) && declMode != 0 && defaultDeclMode != 0 && (!_class || _class->__anon1.__anon1.name))
+{
+if(defaultNameSpace)
+{
+memcpy(name, defaultNameSpace, defaultNameSpaceLen);
+len += defaultNameSpaceLen;
+name[len++] = ':';
+name[len++] = ':';
+}
+if(currentNameSpace)
+{
+memcpy(name + len, currentNameSpace, currentNameSpaceLen);
+len += currentNameSpaceLen;
+name[len++] = ':';
+name[len++] = ':';
+}
+}
+stringLen = strlen(className);
+memcpy(name + len, className, stringLen);
+len += stringLen;
+name[len] = (char)0;
+return _DeclClass(_class, name);
+}
+
+struct Symbol * DeclClass(struct Specifier * _class, const char * name)
+{
+if(_class || strchr(name, ':'))
+return _DeclClass(_class, name);
+else
+return DeclClassAddNameSpace(_class, name);
+}
 
 struct ClassDefinition;
 
@@ -1595,6 +1597,23 @@ struct __ecereNameSpace__ecere__com__NameSpace systemNameSpace;
 
 static struct __ecereNameSpace__ecere__com__Class * __ecereClass_ContextStringPair;
 
+void __ecereMethod_ContextStringPair_OnFree(struct __ecereNameSpace__ecere__com__Class * class, struct ContextStringPair * this)
+{
+(__ecereNameSpace__ecere__com__eSystem_Delete(this->string), this->string = 0);
+(__ecereNameSpace__ecere__com__eSystem_Delete(this->context), this->context = 0);
+}
+
+int __ecereMethod_ContextStringPair_OnCompare(struct __ecereNameSpace__ecere__com__Class * class, struct ContextStringPair * this, struct ContextStringPair * b)
+{
+int result;
+
+result = (this->string && b->string) ? strcmp(this->string, b->string) : (!this->string && b->string) ? 1 : (this->string && !b->string) ? -1 : 0;
+if(result)
+return result;
+result = (this->context && b->context) ? strcmp(this->context, b->context) : (!this->context && b->context) ? 1 : (this->context && !b->context) ? -1 : 0;
+return result;
+}
+
 extern void __ecereNameSpace__ecere__com__PrintLn(struct __ecereNameSpace__ecere__com__Class * class, const void * object, ...);
 
 extern struct __ecereNameSpace__ecere__com__Class * __ecereClass_Identifier;
@@ -1706,23 +1725,6 @@ int origImportType;
 struct __ecereNameSpace__ecere__com__NameSpace privateNameSpace;
 struct __ecereNameSpace__ecere__com__NameSpace publicNameSpace;
 } __attribute__ ((gcc_struct));
-
-void __ecereMethod_ContextStringPair_OnFree(struct __ecereNameSpace__ecere__com__Class * class, struct ContextStringPair * this)
-{
-(__ecereNameSpace__ecere__com__eSystem_Delete(this->string), this->string = 0);
-(__ecereNameSpace__ecere__com__eSystem_Delete(this->context), this->context = 0);
-}
-
-int __ecereMethod_ContextStringPair_OnCompare(struct __ecereNameSpace__ecere__com__Class * class, struct ContextStringPair * this, struct ContextStringPair * b)
-{
-int result;
-
-result = (this->string && b->string) ? strcmp(this->string, b->string) : (!this->string && b->string) ? 1 : (this->string && !b->string) ? -1 : 0;
-if(result)
-return result;
-result = (this->context && b->context) ? strcmp(this->context, b->context) : (!this->context && b->context) ? 1 : (this->context && !b->context) ? -1 : 0;
-return result;
-}
 
 struct Specifier * MkSpecifier(int specifier)
 {
@@ -3052,7 +3054,7 @@ templateString[len++] = '\0';
 symbol = FindClass(templateString);
 if(!symbol && spec->__anon1.__anon1.symbol)
 {
-symbol = _DeclClass(templateString);
+symbol = _DeclClass((((void *)0)), templateString);
 symbol->notYetDeclared = 1;
 }
 if(spec->__anon1.__anon1.symbol)
@@ -3070,10 +3072,19 @@ else
 FreeList(templateArgs, (void *)(FreeTemplateArgument));
 }
 
-struct Symbol * _DeclClass(const char * name)
+struct Symbol * _DeclClass(struct Specifier * _class, const char * name)
 {
-struct Symbol * symbol = FindClass(name);
+struct Symbol * symbol;
+char nameBuffer[1024];
 
+if(_class)
+{
+strcpy(nameBuffer, _class->__anon1.__anon1.name ? _class->__anon1.__anon1.name : "");
+strcat(nameBuffer, "::");
+strcat(nameBuffer, name);
+name = nameBuffer;
+}
+symbol = FindClass(name);
 if(!symbol)
 {
 if(name[0] == ':' && name[1] == ':')
@@ -3081,7 +3092,7 @@ name += 2;
 symbol = __extension__ ({
 struct Symbol * __ecereInstance1 = __ecereNameSpace__ecere__com__eInstance_New(__ecereClass_Symbol);
 
-__ecereInstance1->string = __ecereNameSpace__ecere__sys__CopyString(name), __ecereInstance1;
+__ecereInstance1->string = __ecereNameSpace__ecere__sys__CopyString(name), __ecereInstance1->notYetDeclared = 1, __ecereInstance1;
 });
 if(!__ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_Add(&globalContext->classes, (struct __ecereNameSpace__ecere__sys__BTNode *)symbol))
 __ecereMethod___ecereNameSpace__ecere__sys__OldList_Add((&*excludedSymbols), symbol);
@@ -3292,7 +3303,23 @@ spec->__anon1.__anon1.name = __ecereNameSpace__ecere__sys__CopyString(className)
 }
 }
 else if(symbol)
+{
+char nameSpace[1024];
+char * c = strstr(name, symbol->string);
+
 spec->__anon1.__anon1.name = __ecereNameSpace__ecere__sys__CopyString(symbol->string);
+if(c && c >= name + 2 && c[-1] == ':' && c[-2] == ':')
+{
+if(c > name + 2)
+{
+memcpy(nameSpace, name, c - name - 2);
+nameSpace[c - name] = (char)0;
+spec->__anon1.__anon1.nsSpec = _MkSpecifierName(nameSpace, (((void *)0)), (((void *)0)));
+}
+else
+spec->__anon1.__anon1.nsSpec = _MkSpecifierName((((void *)0)), (((void *)0)), (((void *)0)));
+}
+}
 else
 spec->__anon1.__anon1.name = __ecereNameSpace__ecere__sys__CopyString(name);
 spec->__anon1.__anon1.symbol = symbol;
@@ -5208,9 +5235,9 @@ __ecereNameSpace__ecere__com__eSystem_RegisterFunction("MkClassDefFixed", "Class
 __ecereNameSpace__ecere__com__eSystem_RegisterFunction("MkClassDefDesignerDefaultProperty", "ClassDef MkClassDefDesignerDefaultProperty(Identifier id)", MkClassDefDesignerDefaultProperty, module, 2);
 __ecereNameSpace__ecere__com__eSystem_RegisterFunction("MkClassDefDefaultProperty", "ClassDef MkClassDefDefaultProperty(ecere::sys::OldList defProperties)", MkClassDefDefaultProperty, module, 2);
 __ecereNameSpace__ecere__com__eSystem_RegisterFunction("MkClassDefFunction", "ClassDef MkClassDefFunction(ClassFunction function)", MkClassDefFunction, module, 2);
-__ecereNameSpace__ecere__com__eSystem_RegisterFunction("DeclClassAddNameSpace", "Symbol DeclClassAddNameSpace(const char * className)", DeclClassAddNameSpace, module, 2);
-__ecereNameSpace__ecere__com__eSystem_RegisterFunction("DeclClass", "Symbol DeclClass(const char * name)", DeclClass, module, 2);
-__ecereNameSpace__ecere__com__eSystem_RegisterFunction("_DeclClass", "Symbol _DeclClass(const char * name)", _DeclClass, module, 2);
+__ecereNameSpace__ecere__com__eSystem_RegisterFunction("DeclClassAddNameSpace", "Symbol DeclClassAddNameSpace(Specifier _class, const char * className)", DeclClassAddNameSpace, module, 2);
+__ecereNameSpace__ecere__com__eSystem_RegisterFunction("DeclClass", "Symbol DeclClass(Specifier _class, const char * name)", DeclClass, module, 2);
+__ecereNameSpace__ecere__com__eSystem_RegisterFunction("_DeclClass", "Symbol _DeclClass(Specifier _class, const char * name)", _DeclClass, module, 2);
 __ecereNameSpace__ecere__com__eSystem_RegisterFunction("SetupBaseSpecs", "void SetupBaseSpecs(Symbol symbol, ecere::sys::OldList baseSpecs)", SetupBaseSpecs, module, 2);
 __ecereNameSpace__ecere__com__eSystem_RegisterFunction("MkClass", "ClassDefinition MkClass(Symbol symbol, ecere::sys::OldList baseSpecs, ecere::sys::OldList definitions)", MkClass, module, 2);
 __ecereNameSpace__ecere__com__eSystem_RegisterFunction("MkExpInstance", "Expression MkExpInstance(Instantiation inst)", MkExpInstance, module, 2);

compiler/libec/src/ast.ec
@@ -1424,7 +1424,7 @@ void SetClassTemplateArgs(Specifier spec, OldList templateArgs)
          if(!symbol && spec.symbol)
          {
             // If class was only decl'ed, invoke DeclClass on this templated class as well
-            symbol = _DeclClass(templateString);
+            symbol = _DeclClass(null, templateString);
             symbol.notYetDeclared = true;
          }
          // Add a reference to all templated class to the basic class
@@ -1470,7 +1470,22 @@ Specifier _MkSpecifierName(const char * name, Symbol symbol, OldList templateArg
          }
       }
       else if(symbol)
+      {
+         char nameSpace[1024];
+         char * c = strstr(name, symbol.string);
          spec.name = CopyString(symbol.string);
+         if(c && c >= name + 2 && c[-1] == ':' && c[-2] == ':')
+         {
+            if(c > name + 2)
+            {
+               memcpy(nameSpace, name, c - name - 2);
+               nameSpace[c-name] = 0;
+               spec.nsSpec = _MkSpecifierName(nameSpace, null, null);
+            }
+            else
+               spec.nsSpec = _MkSpecifierName(null, null, null);
+         }
+      }
       else
          spec.name = CopyString(name);
       spec.symbol = symbol;
@@ -1734,12 +1749,12 @@ ClassDef MkClassDefFunction(ClassFunction function)
    return def;
 }
 
-Symbol DeclClassAddNameSpace(const char * className)
+Symbol DeclClassAddNameSpace(Specifier _class, const char * className)
 {
    char name[1024];
    int len = 0, stringLen;
    name[0] = '\0';
-   if((currentNameSpace || defaultNameSpace) && declMode != defaultAccess && defaultDeclMode != defaultAccess)
+   if(className[0] != ':' && (currentNameSpace || defaultNameSpace) && declMode != defaultAccess && defaultDeclMode != defaultAccess && (!_class || _class.name))
    {
       if(defaultNameSpace)
       {
@@ -1760,20 +1775,30 @@ Symbol DeclClassAddNameSpace(const char * className)
    memcpy(name + len, className, stringLen);
    len += stringLen;
    name[len] = 0;
-   return _DeclClass(name);
+   return _DeclClass(_class, name);
 }
 
-Symbol DeclClass(const char * name)
+Symbol DeclClass(Specifier _class, const char * name)
 {
-   if(strchr(name, ':'))
-      return _DeclClass(name);
+   if(_class || strchr(name, ':'))
+      return _DeclClass(_class, name);
    else
-      return DeclClassAddNameSpace(name);
+      return DeclClassAddNameSpace(_class, name);
 }
 
-Symbol _DeclClass(const char * name)
+Symbol _DeclClass(Specifier _class, const char * name)
 {
-   Symbol symbol = FindClass(name);
+   Symbol symbol;
+   char nameBuffer[1024];
+   if(_class)
+   {
+      strcpy(nameBuffer,  _class.name ? _class.name : "");
+      strcat(nameBuffer, "::");
+      strcat(nameBuffer, name);
+      name = nameBuffer;
+   }
+
+   symbol = FindClass(name);
    if(!symbol)
    {
       /*
@@ -1789,7 +1814,7 @@ Symbol _DeclClass(const char * name)
       symbol = Symbol
       {
          string = CopyString(name);
-         // notYetDeclared = true;
+         notYetDeclared = true;
       };
       if(!globalContext.classes.Add((BTNode)symbol))
          excludedSymbols->Add(symbol);

compiler/bootstrap/ecc/bootstrap/ecc.c
@@ -845,6 +845,7 @@ struct __ecereNameSpace__ecere__sys__OldList templatedClasses;
 struct Context * ctx;
 int isIterator;
 struct Expression * propCategory;
+unsigned int mustRegister;
 } __attribute__ ((gcc_struct));
 
 struct __ecereNameSpace__ecere__sys__OldList _excludedSymbols =

compiler/bootstrap/ecere/bootstrap/BinaryTree.c
@@ -92,6 +92,11 @@ extern int strcmp(const char * , const char * );
 
 struct __ecereNameSpace__ecere__sys__StringBTNode;
 
+void __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_FreeString(char * string)
+{
+(__ecereNameSpace__ecere__com__eSystem_Delete(string), string = 0);
+}
+
 struct __ecereNameSpace__ecere__com__Property;
 
 extern void __ecereNameSpace__ecere__com__eInstance_Watch(void *  instance, struct __ecereNameSpace__ecere__com__Property * _property, void *  object, void (*  callback)(void * , void * ));
@@ -206,11 +211,108 @@ void (*  FreeKey)(void *  key);
 
 __attribute__((unused)) static struct __ecereNameSpace__ecere__sys__BinaryTree __ecereNameSpace__ecere__sys__dummy;
 
+int __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_CompareInt(struct __ecereNameSpace__ecere__sys__BinaryTree * this, uintptr_t a, uintptr_t b)
+{
+return (a > b) ? 1 : ((a < b) ? -1 : 0);
+}
+
+int __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_CompareString(struct __ecereNameSpace__ecere__sys__BinaryTree * this, const char * a, const char * b)
+{
+return (a && b) ? strcmp(a, b) : -1;
+}
+
+void __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_Free(struct __ecereNameSpace__ecere__sys__BinaryTree * this)
+{
+if(this->root)
+__ecereMethod___ecereNameSpace__ecere__sys__BTNode_Free(this->root, this->FreeKey);
+this->root = (((void *)0));
+this->count = 0;
+}
+
+struct __ecereNameSpace__ecere__sys__BTNode * __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_FindString(struct __ecereNameSpace__ecere__sys__BinaryTree * this, const char * key)
+{
+return this->root ? __ecereMethod___ecereNameSpace__ecere__sys__BTNode_FindString(this->root, key) : (((void *)0));
+}
+
+struct __ecereNameSpace__ecere__sys__BTNode * __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_FindPrefix(struct __ecereNameSpace__ecere__sys__BinaryTree * this, const char * key)
+{
+return this->root ? __ecereMethod___ecereNameSpace__ecere__sys__BTNode_FindPrefix(this->root, key) : (((void *)0));
+}
+
+struct __ecereNameSpace__ecere__sys__BTNode * __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_FindAll(struct __ecereNameSpace__ecere__sys__BinaryTree * this, uintptr_t key)
+{
+return this->root ? __ecereMethod___ecereNameSpace__ecere__sys__BTNode_FindAll(this->root, key) : (((void *)0));
+}
+
+void __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_Remove(struct __ecereNameSpace__ecere__sys__BinaryTree * this, struct __ecereNameSpace__ecere__sys__BTNode * node)
+{
+struct __ecereNameSpace__ecere__sys__BTNode * parent = node->parent;
+
+if(parent || this->root == node)
+{
+this->root = __ecereMethod___ecereNameSpace__ecere__sys__BTNode_RemoveSwapRight(node);
+this->count--;
+node->parent = (((void *)0));
+}
+}
+
+char * __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_Print(struct __ecereNameSpace__ecere__sys__BinaryTree * this, char * output, int tps)
+{
+output[0] = (char)0;
+if(this->root)
+__ecereMethod___ecereNameSpace__ecere__sys__BTNode_Print(this->root, output, tps);
+return output;
+}
+
+struct __ecereNameSpace__ecere__sys__BTNode * __ecereProp___ecereNameSpace__ecere__sys__BinaryTree_Get_first(struct __ecereNameSpace__ecere__sys__BinaryTree * this)
+{
+return this->root ? __ecereProp___ecereNameSpace__ecere__sys__BTNode_Get_minimum(this->root) : (((void *)0));
+}
+
+struct __ecereNameSpace__ecere__sys__BTNode * __ecereProp___ecereNameSpace__ecere__sys__BinaryTree_Get_last(struct __ecereNameSpace__ecere__sys__BinaryTree * this)
+{
+return this->root ? __ecereProp___ecereNameSpace__ecere__sys__BTNode_Get_maximum(this->root) : (((void *)0));
+}
+
 unsigned int __ecereMethod___ecereNameSpace__ecere__sys__BTNode_Add(struct __ecereNameSpace__ecere__sys__BTNode * this, struct __ecereNameSpace__ecere__sys__BinaryTree * tree, struct __ecereNameSpace__ecere__sys__BTNode * node);
 
 struct __ecereNameSpace__ecere__sys__BTNode * __ecereMethod___ecereNameSpace__ecere__sys__BTNode_Find(struct __ecereNameSpace__ecere__sys__BTNode * this, struct __ecereNameSpace__ecere__sys__BinaryTree * tree, uintptr_t key);
 
 unsigned int __ecereMethod___ecereNameSpace__ecere__sys__BTNode_Check(struct __ecereNameSpace__ecere__sys__BTNode * this, struct __ecereNameSpace__ecere__sys__BinaryTree * tree);
+
+void __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_Delete(struct __ecereNameSpace__ecere__sys__BinaryTree * this, struct __ecereNameSpace__ecere__sys__BTNode * node)
+{
+void * voidNode = node;
+
+__ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_Remove(this, node);
+(__ecereNameSpace__ecere__com__eSystem_Delete(voidNode), voidNode = 0);
+}
+
+unsigned int __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_Add(struct __ecereNameSpace__ecere__sys__BinaryTree * this, struct __ecereNameSpace__ecere__sys__BTNode * node)
+{
+if(!this->CompareKey)
+this->CompareKey = (void *)(__ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_CompareInt);
+if(!this->root)
+this->root = node;
+else if(__ecereMethod___ecereNameSpace__ecere__sys__BTNode_Add(this->root, this, node))
+this->root = __ecereMethod___ecereNameSpace__ecere__sys__BTNode_Rebalance(node);
+else
+return 0;
+this->count++;
+return 1;
+}
+
+struct __ecereNameSpace__ecere__sys__BTNode * __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_Find(struct __ecereNameSpace__ecere__sys__BinaryTree * this, uintptr_t key)
+{
+if(!this->CompareKey)
+this->CompareKey = (void *)(__ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_CompareInt);
+return this->root ? __ecereMethod___ecereNameSpace__ecere__sys__BTNode_Find(this->root, this, key) : (((void *)0));
+}
+
+unsigned int __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_Check(struct __ecereNameSpace__ecere__sys__BinaryTree * this)
+{
+return this->root ? __ecereMethod___ecereNameSpace__ecere__sys__BTNode_Check(this->root, this) : 1;
+}
 
 struct __ecereNameSpace__ecere__com__DataMember;
 
@@ -402,79 +504,6 @@ struct __ecereNameSpace__ecere__com__NameSpace privateNameSpace;
 struct __ecereNameSpace__ecere__com__NameSpace publicNameSpace;
 } __attribute__ ((gcc_struct));
 
-int __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_CompareInt(struct __ecereNameSpace__ecere__sys__BinaryTree * this, uintptr_t a, uintptr_t b)
-{
-return (a > b) ? 1 : ((a < b) ? -1 : 0);
-}
-
-int __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_CompareString(struct __ecereNameSpace__ecere__sys__BinaryTree * this, const char * a, const char * b)
-{
-return (a && b) ? strcmp(a, b) : -1;
-}
-
-void __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_FreeString(char * string)
-{
-(__ecereNameSpace__ecere__com__eSystem_Delete(string), string = 0);
-}
-
-void __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_Free(struct __ecereNameSpace__ecere__sys__BinaryTree * this)
-{
-if(this->root)
-__ecereMethod___ecereNameSpace__ecere__sys__BTNode_Free(this->root, this->FreeKey);
-this->root = (((void *)0));
-this->count = 0;
-}
-
-struct __ecereNameSpace__ecere__sys__BTNode * __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_FindString(struct __ecereNameSpace__ecere__sys__BinaryTree * this, const char * key)
-{
-return this->root ? __ecereMethod___ecereNameSpace__ecere__sys__BTNode_FindString(this->root, key) : (((void *)0));
-}
-
-struct __ecereNameSpace__ecere__sys__BTNode * __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_FindPrefix(struct __ecereNameSpace__ecere__sys__BinaryTree * this, const char * key)
-{
-return this->root ? __ecereMethod___ecereNameSpace__ecere__sys__BTNode_FindPrefix(this->root, key) : (((void *)0));
-}
-
-struct __ecereNameSpace__ecere__sys__BTNode * __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_FindAll(struct __ecereNameSpace__ecere__sys__BinaryTree * this, uintptr_t key)
-{
-return this->root ? __ecereMethod___ecereNameSpace__ecere__sys__BTNode_FindAll(this->root, key) : (((void *)0));
-}
-
-void __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_Remove(struct __ecereNameSpace__ecere__sys__BinaryTree * this, struct __ecereNameSpace__ecere__sys__BTNode * node)
-{
-struct __ecereNameSpace__ecere__sys__BTNode * parent = node->parent;
-
-if(parent || this->root == node)
-{
-this->root = __ecereMethod___ecereNameSpace__ecere__sys__BTNode_RemoveSwapRight(node);
-this->count--;
-node->parent = (((void *)0));
-}
-}
-
-char * __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_Print(struct __ecereNameSpace__ecere__sys__BinaryTree * this, char * output, int tps)
-{
-output[0] = (char)0;
-if(this->root)
-__ecereMethod___ecereNameSpace__ecere__sys__BTNode_Print(this->root, output, tps);
-return output;
-}
-
-unsigned int __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_Check(struct __ecereNameSpace__ecere__sys__BinaryTree * this)
-{
-return this->root ? __ecereMethod___ecereNameSpace__ecere__sys__BTNode_Check(this->root, this) : 1;
-}
-
-struct __ecereNameSpace__ecere__sys__BTNode * __ecereProp___ecereNameSpace__ecere__sys__BinaryTree_Get_first(struct __ecereNameSpace__ecere__sys__BinaryTree * this)
-{
-return this->root ? __ecereProp___ecereNameSpace__ecere__sys__BTNode_Get_minimum(this->root) : (((void *)0));
-}
-
-struct __ecereNameSpace__ecere__sys__BTNode * __ecereProp___ecereNameSpace__ecere__sys__BinaryTree_Get_last(struct __ecereNameSpace__ecere__sys__BinaryTree * this)
-{
-return this->root ? __ecereProp___ecereNameSpace__ecere__sys__BTNode_Get_maximum(this->root) : (((void *)0));
-}
-
 void __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_OnSerialize(struct __ecereNameSpace__ecere__com__Class * class, struct __ecereNameSpace__ecere__sys__BinaryTree * this, struct __ecereNameSpace__ecere__com__Instance * channel)
 {
 __ecereMethod___ecereNameSpace__ecere__com__IOChannel_Serialize(channel, __ecereClass___ecereNameSpace__ecere__sys__BTNode, this->root);
@@ -505,35 +534,6 @@ void __ecereUnregisterModule_BinaryTree(struct __ecereNameSpace__ecere__com__Ins
 
 __ecerePropM___ecereNameSpace__ecere__sys__BinaryTree_first = (void *)0;
 __ecerePropM___ecereNameSpace__ecere__sys__BinaryTree_last = (void *)0;
-}
-
-unsigned int __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_Add(struct __ecereNameSpace__ecere__sys__BinaryTree * this, struct __ecereNameSpace__ecere__sys__BTNode * node)
-{
-if(!this->CompareKey)
-this->CompareKey = (void *)(__ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_CompareInt);
-if(!this->root)
-this->root = node;
-else if(__ecereMethod___ecereNameSpace__ecere__sys__BTNode_Add(this->root, this, node))
-this->root = __ecereMethod___ecereNameSpace__ecere__sys__BTNode_Rebalance(node);
-else
-return 0;
-this->count++;
-return 1;
-}
-
-struct __ecereNameSpace__ecere__sys__BTNode * __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_Find(struct __ecereNameSpace__ecere__sys__BinaryTree * this, uintptr_t key)
-{
-if(!this->CompareKey)
-this->CompareKey = (void *)(__ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_CompareInt);
-return this->root ? __ecereMethod___ecereNameSpace__ecere__sys__BTNode_Find(this->root, this, key) : (((void *)0));
-}
-
-void __ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_Delete(struct __ecereNameSpace__ecere__sys__BinaryTree * this, struct __ecereNameSpace__ecere__sys__BTNode * node)
-{
-void * voidNode = node;
-
-__ecereMethod___ecereNameSpace__ecere__sys__BinaryTree_Remove(this, node);
-(__ecereNameSpace__ecere__com__eSystem_Delete(voidNode), voidNode = 0);
 }
 
 void __ecereRegisterModule_BinaryTree(struct __ecereNameSpace__ecere__com__Instance * module)

commit 3516970ab392efe4b89854a0f475dc3e8775ab61

ide/Debugger/watches: Fix unichar evaluation broken by fa765a838029ec71a86787e95ebcab5fda0aae1e - Corrected reference to unichar full name - Corrected message to say invalid 'Codepoint' rather than 'Keypoint' - Fixed sizeof evaluation for L"strings"

compiler/bootstrap/libec/bootstrap/ast.c
@@ -3700,6 +3700,8 @@ char ch;
 unsigned int escaped = 0;
 char * s = d->initializer->__anon1.exp->__anon1.__anon2.string;
 
+if(s[0] == 'L')
+s++;
 for(c = 1; (ch = s[c]); c++)
 {
 if(ch == '\\' && !escaped)

compiler/libec/src/ast.ec
@@ -979,6 +979,7 @@ Declaration MkDeclaration(OldList specifiers, OldList initDeclarators)
                         char ch;
                         bool escaped = false;
                         char * s = d.initializer.exp.string;
+                        if(s[0] == 'L') s++;
 
                         // MAKE MORE ACCURATE
                         for(c = 1; (ch = s[c]); c++)

ide/src/debugger/Debugger.ec
@@ -3070,7 +3070,7 @@ class Debugger
                         result = true;
                      }
                      else if(wh.type && (wh.type.kind == charType || (wh.type.kind == classType && wh.type._class &&
-                              wh.type._class.registered && !strcmp(wh.type._class.registered.fullName, "ecere::com::unichar"))) )
+                              wh.type._class.registered && !strcmp(wh.type._class.registered.fullName, "unichar"))) )
                      {
                         unichar value;
                         int signedValue;
@@ -3128,7 +3128,7 @@ class Debugger
                         else if(value > 256 || wh.type.kind != charType)
                         {
                            if(value > 0x10FFFF || !GetCharCategory(value))
-                              snprintf(string, sizeof(string), $"Invalid Unicode Keypoint (0x%08X)", value);
+                              snprintf(string, sizeof(string), $"Invalid Unicode Codepoint (0x%08X)", value);
                            else
                               snprintf(string, sizeof(string), "\'%s\' (U+%04X)", charString, value);
                         }