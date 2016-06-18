c78197edff68a03206fe0299cd73893930ec2219
Jerome St-Louis <jerome@ecere.com>
2016-06-18 01:36:27 -0400

    bindings/python: Initial bindings attempts for Python

---
+++ bindings/python/build_ecere.py
@@ -0,0 +1,11 @@
+from cffi import FFI
+ffi = FFI()
+ffi.cdef(open('cffi-ecere.h').read())  
+ffi.set_source("_pyEcere",
+   '#include "ecere.h"',
+   sources = [ "../c/eC.c", "../c/ecere.c" ],
+   include_dirs = ["../c"],
+   libraries = ["ecere"],
+   library_dirs = ["C:/Program Files/Ecere SDK/bin"])
+if __name__ == "__main__":
+   ffi.compile(verbose=True)
