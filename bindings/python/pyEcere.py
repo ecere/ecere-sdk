b81b1a2fc69f50cbe19d73c5a38d082e8cd6a71c
Jerome St-Louis <jerome@ecere.com>
2016-06-18 02:27:31 -0400

    bindings/c;python: Added caption properties; Window::Create(); background color

--- bindings/python/pyEcere.py
+++ bindings/python/pyEcere.py
@@ -1,12 +1,27 @@
 import sys
 from _pyEcere import *
 
-#   lib.PrintLn(lib.class_String, fun.encode('utf8'), ffi.NULL)
+def printLn(args):
+   lib.PrintLn(lib.class_String, args.encode('utf8'), ffi.NULL)
 
-def app_init():
-   app = lib.eC_init(True, len(sys.argv), [ffi.new("char[]", i.encode('utf8')) for i in sys.argv])
-   lib.ecere_init(app)
-   rApp = ffi.new("Instance *"); rApp[0] = app; lib.Instance_evolve(rApp, lib.class_GuiApplication); app = rApp[0]
+class Color:
+   def __init__(self, r = 0, g = 0, b = 0):
+      self.value = ((((r)) << lib.COLOR_r_SHIFT) | ((g) << lib.COLOR_g_SHIFT) | ((b)) << lib.COLOR_b_SHIFT)
+
+   @property
+   def r(self): return ((((self.value)) & lib.COLOR_r_MASK) >> lib.COLOR_r_SHIFT)
+   @r.setter
+   def r(self, value): self.value = ((self.value) & ~(lib.COLOR_r_MASK)) | (((value)) << lib.COLOR_r_SHIFT)
+
+   @property
+   def g(self): return ((((self.value)) & lib.COLOR_g_MASK) >> lib.COLOR_g_SHIFT)
+   @g.setter
+   def g(self, value): self.value = ((self.value) & ~(lib.COLOR_g_MASK)) | (((value)) << lib.COLOR_g_SHIFT)
+
+   @property
+   def b(self): return ((((self.value)) & lib.COLOR_b_MASK) >> lib.COLOR_r_SHIFT)
+   @b.setter
+   def b(self, value): self.value = ((self.value) & ~(lib.COLOR_b_MASK)) | (((value)) << lib.COLOR_b_SHIFT)
 
 class Size:
    def __init__(self, w = 0, h = 0):
@@ -17,11 +32,26 @@ class Size:
 class Instance(object):
    def __init__(self): self.this = ffi.NULL
 
+class Application(Instance):
+   def __init__(self):
+      self.this = lib.eC_init(True, len(sys.argv), [ffi.new("char[]", i.encode('utf8')) for i in sys.argv])
+      lib.ecere_init(self.this)
+
+   def main(self):
+      lib.Application_main(self.this)
+
+class GuiApplication(Application):
+   def __init__(self):
+      Application.__init__(self)
+      rApp = ffi.new("Instance *"); rApp[0] = self.this; lib.Instance_evolve(rApp, lib.class_GuiApplication); self.this = rApp[0]
+
 class Window(Instance):
-   def __init__(self, hasClose = None, clientSize = None):
+   def __init__(self, caption = None, hasClose = None, clientSize = None, background = None):
       self.this = lib.Instance_new(lib.class_Window)
+      if caption != None:      self.caption = caption
       if hasClose != None:     self.hasClose = hasClose
       if clientSize != None:   self.clientSize = clientSize
+      if background != None:   self.background = background
 
    def create(self): lib.Window_create(self.this)
    def modal(self): lib.Window_modal(self.this)
@@ -35,3 +65,13 @@ class Window(Instance):
    def hasClose(self): value = ffi.new("bool *"); lib.Window_get_hasClose(self.this, value); return value
    @hasClose.setter
    def hasClose(self, value): lib.Window_set_hasClose(self.this, value)
+
+   @property
+   def caption(self): value = lib.Window_get_caption(self.this); return ffi.string(value).decode('utf8')
+   @caption.setter
+   def caption(self, value): lib.Window_set_caption(self.this, value.encode('utf8'))
+
+   @property
+   def background(self): value = Color(); lib.Window_get_background(self.this, value.this); return value
+   @background.setter
+   def background(self, value): lib.Window_set_background(self.this, value.value)
