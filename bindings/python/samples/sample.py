0136ef642d398b0191acb50c36fa215766ff0aa2
Jerome St-Louis <jerome@ecere.com>
2016-06-18 03:21:42 -0400

    bindings/python: printLn() variadic example

--- bindings/python/samples/sample.py
+++ bindings/python/samples/sample.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 from pyEcere import *
 app = GuiApplication()
-Window(caption = "Hello, Python!!", hasClose = True, clientSize = Size(640, 480), background = Color(b = 255))
-Window(caption = "Bindings are cool", hasClose = True, clientSize = Size(320, 200), background = Color(255))
+printLn("Testing Variadic Functions!\n", 1, " + ", 2, " = ", 1+2)
+printLn("Pi = ", 3.141592653589)
+
+Window(
+   caption = "Hello, Python!!",
+   hasClose = True,
+   clientSize = Size(640, 480),
+   background = Color(b = 255))
+Window(
+   caption = "Bindings are cool, 詠春 too!",
+   hasClose = True,
+   clientSize = Size(320, 200),
+   background = Color(255))
+
 app.main()
