ecere/ecere-sdk

commit a47aec0a06c2cf299dac23efa877bc1908fe88d7

ecere/com/containers/CustomAVLTree: Freeing Keys (e.g. for AVLTree)

ecere/src/com/containers/CustomAVLTree.ec
@@ -6,6 +6,7 @@ default:
 
 extern int __ecereVMethodID_class_OnCompare;
 extern int __ecereVMethodID_class_OnCopy;
+extern int __ecereVMethodID_class_OnFree;
 private:
 
 enum AddSide : int { compare = 0, left = -1, right = 1};
@@ -573,7 +574,20 @@ public:
       BT item = (BT)_item;
       // THIS SHOULDN'T BE CALLING THE VIRTUAL FUNCTION
       CustomAVLTree::Remove(_item);
+      FreeKey((BT)item);
       delete item;
+   }
+
+   void FreeKey(BT item)
+   {
+      if(class(BT).type == structClass)
+      {
+         // TODO: Make this easier...
+         Class Tclass = class(BT);
+         ((void (*)(void *, void *))(void *)Tclass._vTbl[__ecereVMethodID_class_OnFree])(Tclass, (((byte *)&item.key) + __ENDIAN_PAD(sizeof(void *))));
+      }
+      else
+         delete item.key;
    }
 
    void Free()
@@ -597,6 +611,7 @@ public:
          else
          {
             BT parent = item.parent;
+            FreeKey((BT)item);
             delete item;
             item = parent;
          }

commit a370f655eaa98068a0749842be64b85e76f451a1

ecere/com/Containers/CustomAVLTree: Fixed key class confusion caused by a47aec0a06c2cf299dac23efa877bc1908fe88d7 - NOTE: We may still need a compiler fix on 'delete' operator to use proper template arg (KT rather than BT)

ecere/src/com/containers/CustomAVLTree.ec
@@ -587,7 +587,12 @@ public:
          ((void (*)(void *, void *))(void *)Tclass._vTbl[__ecereVMethodID_class_OnFree])(Tclass, (((byte *)&item.key) + __ENDIAN_PAD(sizeof(void *))));
       }
       else
-         delete item.key;
+      {
+         // TOFIX: delete key; // This indexes the wrong templateArg (BT instead of KT)
+         KT k = item.key;
+         delete k;
+         item.key = (KT)0;
+      }
    }
 
    void Free()