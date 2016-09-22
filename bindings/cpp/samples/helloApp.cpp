a1705a751d7b47edd62b282ec3996c5f175f13dd
Jerome St-Louis <jerome@ecere.com>
2016-09-22 17:04:10 -0400

    bindings/cpp; c: addressed Application class registration issues - New REGISTER_APP_CLASS and EVOLVE_APP macros - Improved Application class registration samples - Added missing GuiApplication::cycle() 'idle' parameter

--- bindings/cpp/samples/helloApp.cpp
+++ bindings/cpp/samples/helloApp.cpp
@@ -2,26 +2,24 @@
 
 #include "eC.hpp"
 
+class MyApp;
+extern MyApp app;
+
 class MyApp : public Application
 {
 public:
    APP_CONSTRUCT(MyApp, Application) { }
-   static void class_registration(CPPClass & _class);
+   REGISTER()
+   {
+      register_main(_class, [](Application & app)
+      {
+         PrintLn(class_String, "C++: Hello, eC", null);
+      });
+      EVOLVE_APP(MyApp, app);
+   }
 };
 
 MyApp app;
-
-void MyApp::class_registration(CPPClass & _class)
-{
-   register_main(_class, [](Application & app)
-   {
-      PrintLn(class_String, "C++: Hello, eC", null);
-   });
-   Instance_evolve(&app.impl, MyApp::_class.impl);
-   _INSTANCE(app.impl, app.impl->_class) = &app;
-   __thisModule = app.impl;
-   app.vTbl = _class.vTbl;
-}
 
 REGISTER_CLASS_DEF(MyApp, Application, app);
 
