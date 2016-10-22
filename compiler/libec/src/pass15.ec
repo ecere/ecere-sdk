ecere/ecere-sdk

commit e8319626af9d771518f5c43870eecceb26027fb1

compiler/libec: (#1042) Fixed missing parentheses indexing Array::array

compiler/libec/src/pass15.ec
@@ -10715,9 +10715,8 @@ void ProcessExpressionType(Expression exp)
                               *newExp = *exp;
                               if(exp.destType) exp.destType.refCount++;
                               if(exp.expType)  exp.expType.refCount++;
-                              exp.type = castExp;
-                              exp.cast.typeName = MkTypeName(specs, MkDeclaratorPointer(MkPointer(null, null), decl));
-                              exp.cast.exp = newExp;
+                              exp.type = bracketsExp;
+                              exp.list = MkListOne(MkExpCast(MkTypeName(specs, MkDeclaratorPointer(MkPointer(null, null), decl)), newExp));
                               //FreeType(exp.expType);
                               //exp.expType = null;
                               //ProcessExpressionType(sourceExp);

commit f24de951c84266dd4ba32bd769d83b33d3947a9e

compiler/libec/pass15: Fixed broken watches on Array::array - Had been broken by e8319626af9d771518f5c43870eecceb26027fb1

compiler/libec/src/pass15.ec
@@ -10729,6 +10729,8 @@ void ProcessExpressionType(Expression exp)
                               if(exp.expType)  exp.expType.refCount++;
                               exp.type = bracketsExp;
                               exp.list = MkListOne(MkExpCast(MkTypeName(specs, MkDeclaratorPointer(MkPointer(null, null), decl)), newExp));
+                              ((Expression)exp.list->first).expType = exp.expType;
+                              if(exp.expType) exp.expType.refCount++;
                               //FreeType(exp.expType);
                               //exp.expType = null;
                               //ProcessExpressionType(sourceExp);