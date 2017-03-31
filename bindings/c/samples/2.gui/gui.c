d7c0ace8be90f3ed3f65025dc6eca8e2f5d366ae
Rejean Loyer <redj@ecere.com>
2017-03-31 01:16:07 -0400

    bgen: add C binding usage samples.

--- bindings/c/samples/2.gui/gui.c
+++ bindings/c/samples/2.gui/gui.c
@@ -1,31 +1,30 @@
-#define MODULE_NAME  "HelloForm"
-// #define __CONSOLE_APP__
+#define MODULE_NAME  "gui"
 
 #include "ecere.h"
 
-static Class * class_HelloForm;
+static Class * CO(HelloWindow);
 
-struct HelloForm { Label label; };
+struct CM(HelloWindow) { Label label; };
 
-typedef Instance HelloForm;
+typedef Instance HelloWindow;
 
-static bool HelloForm_constructor(HelloForm this)
+static bool HelloWindow_constructor(HelloWindow this)
 {
-   struct HelloForm * self = IPTR(this, HelloForm);
+   struct CM(HelloWindow) * self = IPTR(this, HelloWindow);
    Window_set_caption(this, $("My First Ecere/C Bindings App"));
-   Window_set_borderStyle(this, sizable);
+   Window_set_borderStyle(this, BorderStyle_sizable);
    Window_set_size(this, &(Size){ 380, 190 });
    Window_set_hasClose(this, true);
 
-   FontResource font = newi(class_FontResource);
+   FontResource font = newi(FontResource);
    FontResource_set_faceName(font, "Arial");
    FontResource_set_size(font, 30);
 
    /*char tmp[256];
    String s = Instance_onGetString(0, font, tmp, null, null);
-   PrintLn(class_String, s, null);  // Need to terminate with a null!*/
+   PrintLn(CO(String), s, null);  // Need to terminate with a null!*/
 
-   self->label = newi(class_Label);
+   self->label = newi(Label);
    Window_set_parent(self->label, this);
    Window_set_position(self->label, &(Point){ 10, 10 });
    Window_set_font(self->label, font);
@@ -33,16 +32,16 @@ static bool HelloForm_constructor(HelloForm this)
    return true;
 }
 
-static void HelloForm_destructor(HelloForm this)
+static void HelloWindow_destructor(HelloWindow this)
 {
-   struct HelloForm * self = IPTR(this, HelloForm);
+   struct CM(HelloWindow) * self = IPTR(this, HelloWindow);
    deletei(self->label);
 }
 
 GUIAPP_INTRO
 {
-   class_HelloForm = registerClass(app, HelloForm, Window);
-   HelloForm hello = newi(class_HelloForm);
+   class_HelloWindow = registerClass(app, HelloWindow, Window);
+   HelloWindow hello = newi(HelloWindow);
 
    Application_main(app);
 
