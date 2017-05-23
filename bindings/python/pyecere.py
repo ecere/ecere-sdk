87c9e6bfdd352825d8f3544044f45b41928c2694
Jerome St-Louis <jerome@ecere.com>
2017-05-23 02:53:09 -0400

    bindings/python: Improvements to prototype bindings - fixed pyecere.py case - Fixed enums to use prefix - Centralization of code in Instance class and use of base __init__ - Templates and container support - Auto-creating Python side objects from eC instantiation

--- bindings/python/pyecere.py
+++ bindings/python/pyecere.py
@@ -5,6 +5,52 @@ import os
 
 app = ffi.NULL
 
+class Instance:
+   instances = []
+   def __init__(self, templateParams = None, this = None):
+      if this is None:
+         c = type(self)
+         while not hasattr(c, 'pyClass_' + c.__name__):
+            c = c.__bases__[0]
+         pyClass = getattr(c, 'pyClass_' + c.__name__)
+
+         if templateParams is not None:
+            templateName = ffi.string(pyClass.name).decode('utf8') + templateParams
+            if hasattr(c, templateName):
+               template = getattr(c, templateName)
+            else:
+               template = lib.eC_findClass(pyClass.module, templateName.encode('utf8'))
+               setattr(c, templateName, template)
+
+            self.this = lib.Instance_newEx(template, False)
+         else:
+            self.this = lib.Instance_newEx(pyClass, False)
+      else:
+         self.this = this
+      Instance.instances.append(self)
+      self.handle = ffi.new_handle(self)
+      ffi.cast("void **", ffi.cast("char *", self.this) + self.this._class.offset)[0] = self.handle
+
+def pyObject(eCObject):
+   handle = ffi.cast("void **", ffi.cast("char *", eCObject) + eCObject._class.offset)[0]
+   if handle != ffi.NULL: return ffi.from_handle(handle)
+   return None
+
+@ffi.callback("void(Instance)")
+def cb_Instance_destructor(i):
+   instance = pyObject(i)
+   if instance is not None:
+      Instance.instances.remove(instance)
+      instance.handle = 0
+   else:
+      print("No matching instance! for", ffi.string(i._class.name).decode('utf8'))
+
+@ffi.callback("bool(Instance, bool)")
+def cb_Instance_constructor(i, a):
+   s = (ffi.string(i._class.name).decode('utf8'))[2:]
+   if a: globals()[s](this = i)
+   return True
+
 def I18N(s):
    return ffi.string(lib.getTranslatedString(os.path.splitext(os.path.basename(inspect.getfile(sys._getframe(1))))[0].encode('utf8'), s.encode('utf8'), ffi.NULL        )).decode('utf8')
 def I18NC(s, c):
@@ -13,9 +59,10 @@ def I18NC(s, c):
 def convertTypedArgs(args):
    cargs = ()
    for a in args:
-      if type(a) == str:   cargs += (lib.class_String, ffi.new("char[]", a.encode('utf8')))
-      if type(a) == int:   cargs += (lib.class_int,    ffi.new("int *", a))
-      if type(a) == float: cargs += (lib.class_double, ffi.new("double *", a))
+      if type(a) == str:            cargs += (lib.class_String, ffi.new("char[]", a.encode('utf8')))
+      if type(a) == int:            cargs += (lib.class_int,    ffi.new("int *", a))
+      if type(a) == float:          cargs += (lib.class_double, ffi.new("double *", a))
+      if isinstance(a, Instance):   cargs += (a.this._class, a.this)
    return cargs + (ffi.NULL,)
 
 def printLn(*args): lib.printLn(*convertTypedArgs(args))
@@ -64,19 +111,19 @@ class BorderBits:
    def thin(self, value): self.value = ((self.value) & ~(lib.BORDERBITS_thin_MASK)) | (((value)) << lib.BORDERBITS_thin_SHIFT)
 
 class BorderStyle(BorderBits):
-   none         = BorderBits(this = lib.none)
-   contour      = BorderBits(this = lib.contour)
-   fixed        = BorderBits(this = lib.fixed)
-   sizable      = BorderBits(this = lib.sizable)
-   thin         = BorderBits(this = lib.thin)
-   sizableThin  = BorderBits(this = lib.sizableThin)
-   deep         = BorderBits(this = lib.deep)
-   bevel        = BorderBits(this = lib.bevel)
-   sizableDeep  = BorderBits(this = lib.sizableDeep)
-   sizableBevel = BorderBits(this = lib.sizableBevel)
-   fixedDeep    = BorderBits(this = lib.fixedDeep)
-   fixedBevel   = BorderBits(this = lib.fixedBevel)
-   deepContour  = BorderBits(this = lib.deepContour)
+   none         = BorderBits(this = lib.BorderStyle_none)
+   contour      = BorderBits(this = lib.BorderStyle_contour)
+   fixed        = BorderBits(this = lib.BorderStyle_fixed)
+   sizable      = BorderBits(this = lib.BorderStyle_sizable)
+   thin         = BorderBits(this = lib.BorderStyle_thin)
+   sizableThin  = BorderBits(this = lib.BorderStyle_sizableThin)
+   deep         = BorderBits(this = lib.BorderStyle_deep)
+   bevel        = BorderBits(this = lib.BorderStyle_bevel)
+   sizableDeep  = BorderBits(this = lib.BorderStyle_sizableDeep)
+   sizableBevel = BorderBits(this = lib.BorderStyle_sizableBevel)
+   fixedDeep    = BorderBits(this = lib.BorderStyle_fixedDeep)
+   fixedBevel   = BorderBits(this = lib.BorderStyle_fixedBevel)
+   deepContour  = BorderBits(this = lib.BorderStyle_deepContour)
 
 class Color:
    def __init__(self, r = 0, g = 0, b = 0):
@@ -112,18 +159,18 @@ class ColorAlpha:
    def color(self, value): self.value = ((self.value) & ~(lib.COLORALPHA_color_MASK)) | (((value)) << lib.COLORALPHA_color_SHIFT)
 
 class BitmapResource:
-   def __init__(self, fileName = None):
-      self.this = lib.Instance_new(lib.class_BitmapResource)
+   def __init__(self, fileName = None, this = None):
+      Instance.__init__(self, this = this)
       if fileName is not None:      self.fileName = fileName
 
    @property
-   def fileName(self): return ffi.string(lib.FontResource_get_fileName(self.this)).decode('utf8')
+   def fileName(self): return ffi.string(lib.BitmapResource_get_fileName(self.this)).decode('utf8')
    @fileName.setter
    def fileName(self, value): lib.BitmapResource_set_fileName(self.this, value.encode('utf8'))
 
-class FontResource:
-   def __init__(self, faceName = None, size = None, outlineSize = None, outlineFade = None):
-      self.this = lib.Instance_new(lib.class_FontResource)
+class FontResource(Instance):
+   def __init__(self, faceName = None, size = None, outlineSize = None, outlineFade = None, this = None):
+      Instance.__init__(self, this = this)
       if faceName is not None:      self.faceName = faceName
       if size is not None:          self.size = size
       if outlineSize is not None:   self.outlineSize = outlineSize
@@ -166,32 +213,36 @@ class Point:
       self.this.x = x
       self.this.y = y
 
-class Instance:
-   def __init__(self): self.this = ffi.NULL
-
-@ffi.callback("void(Instance)")
-def cb_Window_destructor(w):
-   instance = ffi.from_handle(ffi.cast("void **", ffi.cast("char *", w) + w._class.offset)[0])
-   Window.instances.remove(instance)
-   #print("Instance destroyed now!")
-   instance.handle = 0
-
 class Surface(Instance):
    def __init__(self, this = None):
-      if this is not None:
-         self.this = this
+      Instance.__init__(self, this = this)
 
    def writeTextf(self, x, y, format):
       lib.Surface_writeTextf(self.this, x, y, format.encode('utf8'))
 
 class Application(Instance):
+   def registerClass(module, n):
+      pyClass =  lib.eC_registerClass(lib.ClassType_normalClass, ("Py" + n.__name__).encode('utf8'), n.__name__.encode('utf8'), 8, 0,
+         ffi.cast("bool(*)(void *)", cb_Instance_constructor),
+         ffi.cast("void(*)(void *)", cb_Instance_destructor),
+         module.this, lib.AccessMode_publicAccess, lib.AccessMode_publicAccess);
+      setattr(n, 'pyClass_' + n.__name__, pyClass)
+      pyClass.bindingsClass = ffi.new_handle(n)
+
    def __init__(self):
       self.this = lib.eC_init(ffi.NULL, True, True, len(sys.argv), [ffi.new("char[]", i.encode('utf8')) for i in sys.argv])
       lib.ecere_init(self.this)
-      Window.pyClass_Window         = lib.eC_registerClass(lib.normalClass, "PyWindow"    .encode('utf8'), "Window"     .encode('utf8'), 8, 0, ffi.NULL, ffi.cast("void(*)(void *)", cb_Window_destructor), self.this, lib.publicAccess, lib.publicAccess);
-      Button.pyClass_Button         = lib.eC_registerClass(lib.normalClass, "PyButton"    .encode('utf8'), "Button"     .encode('utf8'), 8, 0, ffi.NULL, ffi.cast("void(*)(void *)", cb_Window_destructor), self.this, lib.publicAccess, lib.publicAccess);
-      Picture.pyClass_Picture       = lib.eC_registerClass(lib.normalClass, "PyPicture"   .encode('utf8'), "Picture"    .encode('utf8'), 8, 0, ffi.NULL, ffi.cast("void(*)(void *)", cb_Window_destructor), self.this, lib.publicAccess, lib.publicAccess);
-      MessageBox.pyClass_MessageBox = lib.eC_registerClass(lib.normalClass, "PyMessageBox".encode('utf8'), "MessageBox" .encode('utf8'), 8, 0, ffi.NULL, ffi.cast("void(*)(void *)", cb_Window_destructor), self.this, lib.publicAccess, lib.publicAccess);
+      self.registerClass(Instance)
+      self.registerClass(Container)
+      self.registerClass(Array)
+      self.registerClass(Surface)
+      self.registerClass(FontResource)
+      self.registerClass(BitmapResource)
+      self.registerClass(Window)
+      self.registerClass(Button)
+      self.registerClass(Picture)
+      self.registerClass(MessageBox)
+      self.registerClass(WindowController)
 
    def main(self):
       lib.Application_main(self.this)
@@ -213,70 +264,65 @@ class Anchor:
       # Would generate conversion properties here instead?
       if left is not None:
          if type(left) == int:
-            self.this.left.type = lib.offset
+            self.this.left.type = lib.AnchorValueType_offset
             self.this.left.distance = left
          elif type(left) == float:
-            self.this.left.type = lib.relative
+            self.this.left.type = lib.AnchorValueType_relative
             self.this.left.distance = left
       if right is not None:
          if type(right) == int:
-            self.this.right.type = lib.offset
+            self.this.right.type = lib.AnchorValueType_offset
             self.this.right.distance = right
          elif type(right) == float:
-            self.this.right.type = lib.relative
+            self.this.right.type = lib.AnchorValueType_relative
             self.this.right.distance = right
       if top is not None:
          if type(top) == int:
-            self.this.top.type = lib.offset
+            self.this.top.type = lib.AnchorValueType_offset
             self.this.top.distance = top
          elif type(top) == float:
-            self.this.top.type = lib.relative
+            self.this.top.type = lib.AnchorValueType_relative
             self.this.top.distance = top
       if bottom is not None:
          if type(bottom) == int:
-            self.this.bottom.type = lib.offset
+            self.this.bottom.type = lib.AnchorValueType_offset
             self.this.bottom.distance = bottom
          elif type(bottom) == float:
-            self.this.bottom.type = lib.relative
+            self.this.bottom.type = lib.AnchorValueType_relative
             self.this.bottom.distance = bottom
       if horz is not None:
          if type(horz) == int:
-            self.this.horz.type = lib.offset
+            self.this.horz.type = lib.AnchorValueType_offset
             self.this.horz.distance = horz
          elif type(horz) == float:
-            self.this.horz.type = lib.relative
+            self.this.horz.type = lib.AnchorValueType_relative
             self.this.horz.distance = horz
       if vert is not None:
          if type(vert) == int:
-            self.this.vert.type = lib.offset
+            self.this.vert.type = lib.AnchorValueType_offset
             self.this.vert.distance = vert
          elif type(vert) == float:
-            self.this.vert.type = lib.relative
+            self.this.vert.type = lib.AnchorValueType_relative
             self.this.vert.distance = vert
 
 @ffi.callback("void(Window, Surface)")
 def cb_Window_onRedraw(w, s):
-   window = ffi.from_handle(ffi.cast("void **", ffi.cast("char *", w) + w._class.offset)[0])
-   surface = Surface(this = s)
-   window.fn_onRedraw(window, surface)
+   window = pyObject(w)
+   window.fn_onRedraw(window, Surface(this = s))
 
 @ffi.callback("void(Window, Surface)")
 def cb_Window_onDrawOverChildren(w, s):
-   window = ffi.from_handle(ffi.cast("void **", ffi.cast("char *", w) + w._class.offset)[0])
-   surface = Surface(this = s)
-   window.fn_onDrawOverChildren(window, surface)
+   window = pyObject(w)
+   window.fn_onDrawOverChildren(window, Surface(this = s))
 
 class Window(Instance):
-   instances = []
-
    def __init__(self,
       parent = None, caption = None, displayDriver = None,
       hasClose = None, hasMinimize = None, hasMaximize = None,
+      controller = None,
       borderStyle = None, clientSize = None, font = None, background = None, foreground = None, position = None):
-      self.this = lib.Instance_new(Window.pyClass_Window)
-      Window.instances.append(self)
-      self.handle = ffi.new_handle(self)
-      ffi.cast("void **", ffi.cast("char *", self.this) + self.this._class.offset)[0] = self.handle
+
+      Instance.__init__(self)
 
       if parent is not None:        self.parent = parent
       if caption is not None:       self.caption = caption
@@ -290,6 +336,7 @@ class Window(Instance):
       if position is not None:      self.position = position
       if background is not None:    self.background = background
       if foreground is not None:    self.foreground = foreground
+      if controller is not None:    self.controller = controller
 
    #def __del__(self):
       #print("Window object is gone!")
@@ -363,6 +410,11 @@ class Window(Instance):
    def font(self, value): lib.Window_set_font(self.this, value.this)
 
    @property
+   def controller(self): value = lib.Window_get_controller(self.this); return Window(this = value)
+   @controller.setter
+   def controller(self, value): lib.Window_set_controller(self.this, value.this)
+
+   @property
    def onRedraw(self): return self.fn_onRedraw
    @onRedraw.setter
    def onRedraw(self, value):
@@ -378,17 +430,13 @@ class Window(Instance):
 
 @ffi.callback("bool(Window, Button, int, int, Modifiers)")
 def cb_Button_notifyClicked(m, b, x, y, mods):
-   button = ffi.from_handle(ffi.cast("void **", ffi.cast("char *", b) + b._class.offset)[0])
-   master = ffi.from_handle(ffi.cast("void **", ffi.cast("char *", m) + m._class.offset)[0])
-   button.fn_notifyClicked(master, button, x, y, mods)
+   button = pyObject(b)
+   button.fn_notifyClicked(pyObject(m), button, x, y, mods)
    return True
 
 class Button(Window):
    def __init__(self, parent = None, caption = None, position = None, font = None, notifyClicked = None):
-      self.this = lib.Instance_new(Button.pyClass_Button)
-      Window.instances.append(self)
-      self.handle = ffi.new_handle(self)
-      ffi.cast("void **", ffi.cast("char *", self.this) + self.this._class.offset)[0] = self.handle
+      Instance.__init__(self)
 
       if parent is not None:        self.parent = parent
       if caption is not None:       self.caption = caption
@@ -413,10 +461,7 @@ class Button(Window):
 
 class Picture(Window):
    def __init__(self, parent = None, caption = None, image = None, position = None, anchor = None):
-      self.this = lib.Instance_new(Picture.pyClass_Picture)
-      Window.instances.append(self)
-      self.handle = ffi.new_handle(self)
-      ffi.cast("void **", ffi.cast("char *", self.this) + self.this._class.offset)[0] = self.handle
+      Instance.__init__(self)
 
       if parent is not None:       self.parent = parent
       if caption is not None:      self.caption = caption
@@ -431,10 +476,7 @@ class Picture(Window):
 
 class MessageBox(Window):
    def __init__(self, parent = None, caption = None, contents = None, position = None):
-      self.this = lib.Instance_new(MessageBox.pyClass_MessageBox)
-      Window.instances.append(self)
-      self.handle = ffi.new_handle(self)
-      ffi.cast("void **", ffi.cast("char *", self.this) + self.this._class.offset)[0] = self.handle
+      Instance.__init__(self)
 
       if parent is not None:       self.parent = parent
       if caption is not None:      self.caption = caption
@@ -445,3 +487,44 @@ class MessageBox(Window):
    def contents(self): value = lib.MessageBox_get_contents(self.this); return ffi.string(value).decode('utf8')
    @contents.setter
    def contents(self, value): lib.MessageBox_set_contents(self.this, value.encode('utf8'))
+
+class WindowController(Instance):
+   def __init__(self, templateParams = None):
+      Instance.__init__(self, templateParams)
+
+def TA(a):
+   if type(a) == int:            return lib.TAi(a)
+   if type(a) == float:          return lib.TAf(a)
+   if isinstance(a, Instance):   return lib.TAo(a.this)
+   return 0
+
+class Container(Instance):
+   def __init__(self, templateParams = None, copySrc = None):
+      Instance.__init__(self, templateParams)
+      if copySrc is not None: self.copySrc = copySrc
+
+   def add(self, value):
+      lib.Container_add(self.this, TA(value))
+
+   @property
+   def copySrc(self): return lib.Container_get_copySrc(self.this)
+   @copySrc.setter
+   def copySrc(self, value):
+      if isinstance(value, Container): lib.Container_set_copySrc(self.this, value)
+      if isinstance(value, list):
+         bic = ffi.new("BuiltInContainer *")
+         bic._vTbl = lib.class_BuiltInContainer._vTbl
+         bic._class = lib.class_BuiltInContainer
+         bic._refCount = 0
+         bic.count = len(value)
+         if type(value[0]) == int:
+            bic.data = ffi.new("int[]", value)
+            bic.type = lib.class_int
+         if type(value[0]) == float:
+            bic.data = ffi.new("double[]", value)
+            bic.type = lib.class_double
+         lib.Container_set_copySrc(self.this, ffi.cast("Container", bic))
+
+class Array(Container):
+   def __init__(self, templateParams = None, copySrc = None):
+      Container.__init__(self, templateParams, copySrc)
