3368e835b881108e83a68483986f0c732f784b81
Jerome St-Louis <jerome@ecere.com>
2017-06-24 23:10:49 -0400

    ecere/gfx/newFonts/cc/mmthread: Fixed WIN32_LEAN_AND_MEAN warnings

--- ecere/src/gfx/newFonts/cc/mmthread.h
+++ ecere/src/gfx/newFonts/cc/mmthread.h
@@ -340,7 +340,9 @@ static inline int mtSignalWaitTime( mtSignal *signal, mtMutex *mutex, unsigned l
 
 /* Windows threads */
 
+ #ifndef WIN32_LEAN_AND_MEAN
  #define WIN32_LEAN_AND_MEAN
+ #endif
  #include <windows.h>
  #if MT_MSVC
   #include <intrin.h>
