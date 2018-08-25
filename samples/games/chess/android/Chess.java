c04b48ba3874bc5800583b9fabd5b55eba7b2cc2
Jerome St-Louis <jerome@ecere.com>
2018-08-25 01:57:02 -0400

    samples/chess (Android): Updated for latest SDK

---
+++ samples/games/chess/android/Chess.java
@@ -0,0 +1,10 @@
+package com.ecere.Chess;
+
+public class Chess extends android.app.NativeActivity
+{
+   static
+   {
+      System.loadLibrary("ecere");
+      System.loadLibrary("Chess");
+   }
+}
