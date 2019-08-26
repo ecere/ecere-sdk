9594c7a630bf9fdca92b2a0dda80faf43ba37ef9
Diyane Fernando <diyane@ecere.com>
2019-08-26 14:35:52 -0400

    cppPropMacroExpansionTest: last changes.

--- cppPropMacroExpansion/TEST.cpp
+++ cppPropMacroExpansion/TEST.cpp
@@ -1,42 +1,11 @@
 #define SELF(c, n)  __attribute__((unused)) c * self = ((c *)(((char *)this) + 0x10 - (char *)&((c *)0x10)->n))
 
 #define propertyProto(n, sg) struct n ## Prop { n ## Prop() { }; int _[0]; sg } n;
+
 #define propertyImpl(sg) sg
 
-#define _set(t, n, c, d) \
-   inline t operator= (t v) \
-   { \
-      SELF(c, n); \
-      d; \
-      return v; \
-   } \
-
-#define set(t, n, c, d) \
-   _set(t, n, c, d) \
-   inline n ## Prop & operator= (n ## Prop & prop) \
-   { \
-      SELF(c, n); \
-      t v = prop; \
-      d; \
-      return prop; \
-   }
-
-#define get(t, n, c, d) inline operator t () const { SELF(c, n); d; }
-
-
-// prototypes
-
 #define _setProto(t, n, c, d) \
-   t operator= (t v); 
-
-#define setProto(t, n, c, d) \
-   _setProto(t, n, c, d) \
-   n ## Prop & operator= (n ## Prop & prop);
-
-#define getProto(t, n, c, d) \
-   operator t () const;
-
-// implementations
+   t operator= (t v); \
 
 #define _setImpl(t, n, c, d) \
    t c::n ## Prop::operator= (t v) \
@@ -45,6 +14,10 @@
       d; \
       return v; \
    } \
+
+#define setProto(t, n, c, d) \
+   _setProto(t, n, c, d) \
+   n ## Prop & operator= (n ## Prop & prop);
 
 #define setImpl(t, n, c, d) \
    _setImpl(t, n, c, d) \
@@ -56,9 +29,9 @@
       return prop; \
    }
 
+#define getProto(t, n, c, d) operator t () const;
+
 #define getImpl(t, n, c, d) c::n ## Prop::operator t () const { SELF(c, n); d; }
-
-
 
 class test
 {
