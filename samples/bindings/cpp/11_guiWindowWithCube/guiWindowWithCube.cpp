b711685f4e7e010cb04492276aab0859752fd43d
Rejean Loyer <redj@ecere.com>
2019-12-25 15:44:27 -0500

    samples/bindings/cpp: work on cube sample.

--- samples/bindings/cpp/11_guiWindowWithCube/guiWindowWithCube.cpp
+++ samples/bindings/cpp/11_guiWindowWithCube/guiWindowWithCube.cpp
@@ -21,11 +21,14 @@ Light light
    orientation = Euler { pitch = 10, yaw = 30 };
 };
 */
+C(Light) light { };
 
 class HelloCube : public Window
 {
 public:
-   // Cube cube;
+   // C(Cube) * cube;
+   Cube cube;
+   // Object object;
 
    CONSTRUCT(HelloCube, Window)
    {
@@ -42,10 +45,24 @@ public:
 
       onLoadGraphics = [](Window & window) -> C(bool)
       {
-         // cube.create(self.displaySystem);
+         HelloCube & self = (HelloCube &)window;
+         C(Transform) transform;
+         // DisplaySystem & ds = self.displaySystem;
+         // self.cube.create(ds);
+         self.cube.create(self.displaySystem);
+         Object_get_transform((C(Object)*)self.cube.impl, &transform);
+         transform.scaling = (C(Vector3Df)){ 100, 100, 100 };
+         // transform.orientation = { 100, 100, 100 };
+         {
+            C(Quaternion) orientation;
+            C(Euler) eul = { Degrees(50), Degrees(30), Degrees(50) };
+            Euler_to_Quaternion(&eul, &orientation);
+            transform.orientation = orientation;
+         }
          // self.cube.transform.scaling = { 100, 100, 100 };
          // self.cube.transform.orientation = Euler { 50, 30, 50 };
-         // self.cube.updateTransform();
+         // self.object.updateTransform();
+         self.cube.updateTransform();
          return true;
       };
 
@@ -58,10 +75,10 @@ public:
 
       onRedraw = [](Window & window, Surface & surface)
       {
-         // HelloCube & self = (HelloCube &)window;
-         // Display & display = self.display;
+         HelloCube & self = (HelloCube &)window;
+         Display & display = self.display;
          surface.clear(ClearType_depthBuffer);
-         // self.display.setLight(0, light);
+         // display.setLight(0, light);
          // self.display.setCamera(surface, camera);
          // self.display.drawObject(self.cube);
          // display.setCamera(surface, null);
