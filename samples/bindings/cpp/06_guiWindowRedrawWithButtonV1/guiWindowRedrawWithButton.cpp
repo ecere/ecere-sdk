0fedfdfcb2e2c29933cbddcc2cfd8a2c78dd06c9
Rejean Loyer <redj@ecere.com>
2020-02-22 02:24:50 -0500

    samples/bindings/cpp: messy update to C++ samples

--- samples/bindings/cpp/06_guiWindowRedrawWithButtonV1/guiWindowRedrawWithButton.cpp
+++ samples/bindings/cpp/06_guiWindowRedrawWithButtonV1/guiWindowRedrawWithButton.cpp
@@ -7,21 +7,26 @@ class HelloForm : public Window
 public:
    Button button;
 
+   REGISTER()
+   {
+      REG_Window(HelloForm);
+      DESTRUCT(HelloForm) = [](HelloForm & self) { printf("It's the end my friend!\n"); system("pause"); };
+   }
    CONSTRUCT(HelloForm, Window)
    {
       caption = $("Sample App using Ecere Toolkit/C++ Bindings");
-      borderStyle = BorderStyle_sizable;
+      borderStyle = BorderStyle::sizable;
       clientSize = { 640, 480 };
       hasClose = true;
       hasMaximize = true;
       hasMinimize = true;
-      background = SystemColor_formColor;
+      background = SystemColor::formColor;
       font = { "Arial", 30 };
 
       button.parent = this;
       button.position = { 200, 200 };
       button.caption = $("Yay!!");
-      button.notifyClicked = [](Window & owner, Button & btn, int x, int y, Modifiers mods) -> C(bool)
+      button.notifyClicked = [](Window & owner, Button & btn, int x, int y, Modifiers mods) -> bool
       {
          HelloForm & self = (HelloForm &)owner;
          MessageBox msgBox;
@@ -32,14 +37,19 @@ public:
       };
    }
 
-   REGISTER()
+   void onRedraw(Surface & surface)
    {
-      Window::class_registration(_class);
-
-      register_onRedraw(_class, [](Window & w, Surface & surface) { surface.writeText/*f*/(100, 100, "simple", 6); });
-
-      DESTRUCT(HelloForm) = [](HelloForm & self) { printf("It's the end my friend!\n"); };
+      surface.writeText/*f*/(100, 100, "simple", 6);
    }
+
+   // REGISTER()
+   // {
+      // Window::class_registration(_class);
+
+      // register_onRedraw(_class, [](Window & w, Surface & surface) { surface.writeText/*f*/(100, 100, "simple", 6); });
+
+      // DESTRUCT(HelloForm) = [](HelloForm & self) { printf("It's the end my friend!\n"); };
+   // }
 };
 
 GuiApplication app;
--- samples/bindings/cpp/09_guiWindowRedrawWithButtonV4/guiWindowRedrawWithButton.cpp
+++ samples/bindings/cpp/09_guiWindowRedrawWithButtonV4/guiWindowRedrawWithButton.cpp
@@ -10,19 +10,19 @@ public:
    CONSTRUCT(HelloForm, Window)
    {
       caption = $("Sample App using Ecere Toolkit/C++ Bindings");
-      borderStyle = BorderStyle_sizable;
+      borderStyle = BorderStyle::sizable;
       displayDriver = "OpenGL";
       clientSize = { 640, 480 };
       hasClose = true;
       hasMaximize = true;
       hasMinimize = true;
-      background = SystemColor_formColor;
+      background = SystemColor::formColor;
       font = { "Arial", 30 };
 
       button.parent = this;
       button.position = { 200, 200 };
       button.caption = $("Yay!!");
-      button.notifyClicked = [](Window & owner, Button & btn, int x, int y, Modifiers mods) -> C(bool)
+      button.notifyClicked = [](Window & owner, Button & btn, int x, int y, Modifiers mods) -> bool
       {
          HelloForm & self = (HelloForm &)owner;
          MessageBox msgBox;
