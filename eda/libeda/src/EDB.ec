ecere/ecere-sdk

commit 6400c5e5499fe1ced58adb57a20a7743423a0199

EDA/gui: Added get property to editor to avoid confusion with DataBox::editor

eda/libeda/src/gui.ec
@@ -384,6 +384,7 @@ public:
          value.AddFieldEditor(this);
          row = value.editRow;
       }
+      get { return (EditSection)master; }
    };
 
    property Field field

commit cfdfbaab6c1237c05c8bfb059dedab92b28fab6c

eda/gui: deconflict 'editor' members of FieldBox and FieldDataBox - This solves crashes resulting from property 'get' introduced in 6400c5e5499fe1ced58adb57a20a7743423a0199 - class FieldBox : DataBox is defined in eda/FieldBox.ec - class FieldDataBox : DataBox is defined in eda/gui.ec - Renamed 'editor' member of both to tableEditor to avoid confusion with DropBox::editor - Also renamed 'editor' member of ListSection (an EditSection) to 'editSection' - samples/db: Updated - Fixed some warnings

eda/libeda/src/EDB.ec
@@ -16,7 +16,7 @@ EDBIndexOptions indexOptions { saveIndex = true, deleteIndex = false; };
 
 __attribute__((unused)) static void UnusedFunction()
 {
-   int a;
+   int a = 0;
    a.OnGetString(0,0,0);
    a.OnFree();
    a.OnCopy(null);

eda/libeda/src/gui.ec
@@ -375,7 +375,7 @@ public:
       set { row = value; }
       get { return row; }
    }
-   property EditSection editor
+   property EditSection tableEditor
    {
       set
       {
@@ -670,11 +670,11 @@ public class ListSection : Group
    size = { 710, 287 };
    anchor = { left = sgs, top = 32 + sgs * 3, bottom = 55 + sgs * 3 };
 public:
-   property EditSection editor
+   property EditSection editSection
    {
       set
       {
-         editor = value;
+         editSection = value;
          value.list = this;
       }
    }
@@ -697,7 +697,7 @@ public:
                indexedFields[0] = { fldId };
                table.Index(1, indexedFields);
 
-               editor.editRow.tbl = table;
+               editSection.editRow.tbl = table;
 
                RefillList();
             }
@@ -713,16 +713,16 @@ public:
 
    bool OnClose(bool parentClosing)
    {
-      if(editor && editor.modifiedDocument)
+      if(editSection && editSection.modifiedDocument)
       {
          switch(NotifySaveConfirmation(master, this))
          {
             case cancel:
                return false;
             case yes:
-               editor.EditSave();
+               editSection.EditSave();
             case no:
-               editor.modifiedDocument = false;
+               editSection.modifiedDocument = false;
                break;
          }
       }
@@ -739,7 +739,7 @@ public:
          delete r;
       }
       list.Sort(null, 1);
-      editor.modifiedDocument = false;
+      editSection.modifiedDocument = false;
    }
 
    virtual void Window::NotifyRefillList(ListSection listSection, Row r)
@@ -775,7 +775,7 @@ public:
       bool NotifyClicked(Button button, int x, int y, Modifiers mods)
       {
          list.NotifySelect(this, list, null, 0);
-         if(!editor.modifiedDocument)
+         if(!editSection.modifiedDocument)
          {
             Id id; // = table.rowsCount + 1; // this is bad with deleted rows, won't work, how to have unique id?
             Row r { table };
@@ -788,7 +788,7 @@ public:
             else
                id = 1;
 
-            editor.EditClear();
+            editSection.EditClear();
             {
                bool active = true;
                r.Add();
@@ -843,9 +843,9 @@ public:
             {
                NotifyDeleting(master, this);
 
-               editor.editRow.Delete();
+               editSection.editRow.Delete();
                list.DeleteRow(list.currentRow);
-               editor.EditClear();
+               editSection.EditClear();
 
                NotifyDeleted(master, this);
 
@@ -859,7 +859,7 @@ public:
 
    bool FilterNotifyChanged(DataBox dataBox, bool closeDropDown)
    {
-      editor.EditClear();
+      editSection.EditClear();
       RefillList();
       if(list.firstRow)
          SelectListRow(list.firstRow);
@@ -877,7 +877,7 @@ public:
          bool result = true;
          if(/*row && */row != lastRow)
          {
-            if(editor.modifiedDocument)
+            if(editSection.modifiedDocument)
             {
                if(row)
                   list.currentRow = lastRow;
@@ -887,9 +887,9 @@ public:
                   case cancel:
                      return false;
                   case yes:
-                     editor.EditSave();
+                     editSection.EditSave();
                   case no:
-                     editor.modifiedDocument = false;
+                     editSection.modifiedDocument = false;
                      list.currentRow = row;
                      break;
                }
@@ -912,11 +912,11 @@ public:
 
          if(list.currentRow != row)
             list.currentRow = row;
-         if(editor.editRow.Find(fldId, middle, nil, id))
+         if(editSection.editRow.Find(fldId, middle, nil, id))
          {
-            editor.listRow = row;
+            editSection.listRow = row;
             NotifySelectListRow(master, this, id);
-            editor.EditLoad();
+            editSection.EditLoad();
          }
       }
       // Logf("SelectListRow took %f seconds\n", GetTime() - startTime);
@@ -931,12 +931,12 @@ public:
 
    void RefreshState()
    {
-      if(editor)
+      if(editSection)
       {
-         editor.btnSave.disabled = !list.currentRow;
-         editor.btnReload.disabled = !list.currentRow;
+         editSection.btnSave.disabled = !list.currentRow;
+         editSection.btnReload.disabled = !list.currentRow;
          btnDelete.disabled = !list.currentRow;
-         editor.disabled = !list.firstRow;
+         editSection.disabled = !list.firstRow;
       }
    }
 
@@ -953,12 +953,12 @@ public:
    {
       OnResize(clientSize.w, clientSize.h);
       SelectFirst();
-      if(editor) editor.modifiedDocument = false;
+      if(editSection) editSection.modifiedDocument = false;
       return Window::OnPostCreate();
    }
 
 private:
-   EditSection editor;
+   EditSection editSection;
    Table table;
    DataRow lastRow;
 }

eda/libeda/src/gui/TableEditor.ec
@@ -469,14 +469,14 @@ public:
                {
                   // todo: make this work for all types, although this is meant to be an id field
                   Id id = 0;
-                  TableEditor editor = eInstance_New(f.editorClass);
-                  incref editor;
-                  editor.parent = f.parentWindow;
-                  editor.master = this;
-                  dynamicLookupTableEditors.Add(editor);
-                  editor.Create();
+                  TableEditor tableEditor = eInstance_New(f.editorClass);
+                  incref tableEditor;
+                  tableEditor.parent = f.parentWindow;
+                  tableEditor.master = this;
+                  dynamicLookupTableEditors.Add(tableEditor);
+                  tableEditor.Create();
                   row.GetData(f.lookupIdField, id);
-                  editor.Select(id);
+                  tableEditor.Select(id);
                }
                delete row;
             }

eda/libeda/src/gui/controls/FieldBox.ec
@@ -12,20 +12,20 @@ public class FieldBox : DataBox
 
    Field field;
    int64 dataHolder; // THERE SEEMS TO BE A BUG WHEN ACCESSING row ACROSS .so
-   TableEditor editor;
+   TableEditor tableEditor;
 
    property Row row
    {
       get
       {
          Row result = null;
-         if(field && editor)
+         if(field && tableEditor)
          {
-            if(editor.table == field.table)
-               result = editor.editRow;
-            else if(editor.lookups)
+            if(tableEditor.table == field.table)
+               result = tableEditor.editRow;
+            else if(tableEditor.lookups)
             {
-               Lookup lookup = editor.lookups[field.table]; // Map<Table, Lookup> limits to single lookup per table
+               Lookup lookup = tableEditor.lookups[field.table]; // Map<Table, Lookup> limits to single lookup per table
                if(lookup.valueField && lookup.findField && lookup.row && !lookup.row.nil)
                   result = lookup.row;
             }
@@ -34,17 +34,16 @@ public class FieldBox : DataBox
       }
    }
 
-   // DataBox has a member called editor as well?
-   // would like to rename TableEditor to TableControl anyway
-   public property TableEditor editor
+   // rename TableEditor to TableControl?
+   public property TableEditor tableEditor
    {
       set
       {
-         if(value != editor)
+         if(value != tableEditor)
          {
-            if(editor)
-               editor.RemoveFieldBox(this);
-            editor = value;
+            if(tableEditor)
+               tableEditor.RemoveFieldBox(this);
+            tableEditor = value;
             if(value)
                value.AddFieldBox(this);
          }
@@ -54,13 +53,13 @@ public class FieldBox : DataBox
    watch(parent)
    {
       if(eClass_IsDerived(parent._class, class(TableEditor)))
-         property::editor = (TableEditor)parent;
+         property::tableEditor = (TableEditor)parent;
    };
 
    watch(master)
    {
       if(eClass_IsDerived(master._class, class(TableEditor)))
-         property::editor = (TableEditor)master;
+         property::tableEditor = (TableEditor)master;
    };
 
    public property Field field

eda/libeda/src/idList.ec
@@ -16,7 +16,7 @@ extern int __ecereVMethodID_class_OnFree;
 
 __attribute__((unused)) static void UnusedFunction()
 {
-   int a;
+   int a = 0;
    a.OnGetString(0,0,0);
    a.OnFree();
    a.OnCopy(null);

extras/gui/genericEditor.ec
@@ -31,7 +31,7 @@ class GenericEditor : Window
 
          Clear();
 
-         editor.table = value;
+         editSection.table = value;
          if(!list.fldName && value)
             list.fldName = value.FindField("Name");
          list.table = value;
@@ -40,8 +40,8 @@ class GenericEditor : Window
             if(strcmpi(f.name, "ID"))
             {
                int h = 20;
-               FieldDataBox box { editor = editor, field = f, text = f.name, size = { h = h }, anchor = { left = 20, top = y + 16, right = 20 } };
-               Label label { editor.editArea, position = { 20, y }, labeledWindow = box };
+               FieldDataBox box { tableEditor = editSection, field = f, text = f.name, size = { h = h }, anchor = { left = 20, top = y + 16, right = 20 } };
+               Label label { editSection.editArea, position = { 20, y }, labeledWindow = box };
                if(eClass_IsDerived(f.type, class(DataList)) || eClass_IsDerived(f.type, class(IdList)) || eClass_IsDerived(f.type, class(MultiLineString)))
                {
                   h = 70;
@@ -64,10 +64,10 @@ class GenericEditor : Window
 
    ListSection list
    {
-      this, editor = editor,
+      this, editSection = editSection,
       anchor = { left = 0, top = 0, bottom = 0, right = 0.5 }
    };
-   EditSection editor
+   EditSection editSection
    {
       this,
       anchor = { left = 0.5, top = 0, bottom = 0, right = 0 }

samples/db/MedDB/mainForm.ec
@@ -57,11 +57,11 @@ class RestrictionEditor : Window
 {
    tabCycle = true;
 
-   ListSection list { this, editor = editor, fldName = dbfield("Restrictions", name), table = dbtable("Restrictions"), anchor = { left = 0, top = 0, bottom = 0, right = 0.5 } };
-   EditSection editor { this, table = dbtable("Restrictions"), anchor = { left = 0.5, top = 0, bottom = 0, right = 0 } };
+   ListSection list { this, editSection = editSection, fldName = dbfield("Restrictions", name), table = dbtable("Restrictions"), anchor = { left = 0, top = 0, bottom = 0, right = 0.5 } };
+   EditSection editSection { this, table = dbtable("Restrictions"), anchor = { left = 0.5, top = 0, bottom = 0, right = 0 } };
 
-   FieldDataBox boxName { editor = editor, field = dbfield("Restrictions", name), caption = "Name", size = { 174, 20 }, position = { 20, 32 } };
-   Label label1 { editor.editArea, position = { 20, 8 }, labeledWindow = boxName };
+   FieldDataBox boxName { tableEditor = editSection, field = dbfield("Restrictions", name), caption = "Name", size = { 174, 20 }, position = { 20, 32 } };
+   Label label1 { editSection.editArea, position = { 20, 8 }, labeledWindow = boxName };
 };
 
 class ClassesEditor : Window
@@ -69,22 +69,22 @@ class ClassesEditor : Window
    tabCycle = true;
    background = activeBorder;
 
-   ListSection list { this, editor = editor, fldName = dbfield("DrugClasses", name), table = dbtable("DrugClasses"), anchor = { left = 0, top = 0, bottom = 0, right = 0.5 } };
-   EditSection editor { this, table = dbtable("DrugClasses"), anchor = { left = 0.5, top = 0, bottom = 0, right = 0 } };
+   ListSection list { this, editSection = editSection, fldName = dbfield("DrugClasses", name), table = dbtable("DrugClasses"), anchor = { left = 0, top = 0, bottom = 0, right = 0.5 } };
+   EditSection editSection { this, table = dbtable("DrugClasses"), anchor = { left = 0.5, top = 0, bottom = 0, right = 0 } };
 
-   FieldDataBox boxName { editor = editor, field = dbfield("DrugClasses", name), caption = "Name", size = { 174, 20 }, position = { 20, 32 } };
-   Label label1 { editor.editArea, position = { 20, 8 }, labeledWindow = boxName };
+   FieldDataBox boxName { tableEditor = editSection, field = dbfield("DrugClasses", name), caption = "Name", size = { 174, 20 }, position = { 20, 32 } };
+   Label label1 { editSection.editArea, position = { 20, 8 }, labeledWindow = boxName };
 };
 
 class SubClassesEditor : Window
 {
    tabCycle = true;
    background = activeBorder;
-   ListSection list { this, editor = editor, fldName = dbfield("SubClasses", name), table = dbtable("SubClasses"), anchor = { left = 0, top = 0, bottom = 0, right = 0.5 } };
-   EditSection editor { this, table = dbtable("SubClasses"), anchor = { left = 0.5, top = 0, bottom = 0, right = 0 } };
+   ListSection list { this, editSection = editSection, fldName = dbfield("SubClasses", name), table = dbtable("SubClasses"), anchor = { left = 0, top = 0, bottom = 0, right = 0.5 } };
+   EditSection editSection { this, table = dbtable("SubClasses"), anchor = { left = 0.5, top = 0, bottom = 0, right = 0 } };
 
-   FieldDataBox boxName { editor = editor, field = dbfield("SubClasses", name), caption = "Name", size = { 174, 20 }, position = { 20, 32 } };
-   Label label1 { editor.editArea, position = { 20, 8 }, labeledWindow = boxName };
+   FieldDataBox boxName { tableEditor = editSection, field = dbfield("SubClasses", name), caption = "Name", size = { 174, 20 }, position = { 20, 32 } };
+   Label label1 { editSection.editArea, position = { 20, 8 }, labeledWindow = boxName };
 };
 
 class FormesEditor : Window
@@ -92,49 +92,49 @@ class FormesEditor : Window
    tabCycle = true;
    background = activeBorder;
 
-   ListSection list { this, editor = editor, fldName = dbfield("Forms", name), table = dbtable("Forms"), anchor = { left = 0, top = 0, bottom = 0, right = 0.5 } };
-   EditSection editor { this, table = dbtable("Forms"), anchor = { left = 0.5, top = 0, bottom = 0, right = 0 } };
+   ListSection list { this, editSection = editSection, fldName = dbfield("Forms", name), table = dbtable("Forms"), anchor = { left = 0, top = 0, bottom = 0, right = 0.5 } };
+   EditSection editSection { this, table = dbtable("Forms"), anchor = { left = 0.5, top = 0, bottom = 0, right = 0 } };
 
-   FieldDataBox boxName { editor = editor, field = dbfield("Forms", name), caption = "Name", size = { 174, 20 }, position = { 20, 32 } };
-   Label label1 { editor.editArea, position = { 20, 8 }, labeledWindow = boxName };
+   FieldDataBox boxName { tableEditor = editSection, field = dbfield("Forms", name), caption = "Name", size = { 174, 20 }, position = { 20, 32 } };
+   Label label1 { editSection.editArea, position = { 20, 8 }, labeledWindow = boxName };
 };
 
 class UnitesEditor : Window
 {
    tabCycle = true;
    background = activeBorder;
-   ListSection list { this, editor = editor, fldName = dbfield("Units", name), table = dbtable("Units"), anchor = { left = 0, top = 0, bottom = 0, right = 0.5 } };
-   EditSection editor { this, table = dbtable("Units"), anchor = { left = 0.5, top = 0, bottom = 0, right = 0 } };
+   ListSection list { this, editSection = editSection, fldName = dbfield("Units", name), table = dbtable("Units"), anchor = { left = 0, top = 0, bottom = 0, right = 0.5 } };
+   EditSection editSection { this, table = dbtable("Units"), anchor = { left = 0.5, top = 0, bottom = 0, right = 0 } };
 
-   FieldDataBox boxName { editor = editor, field = dbfield("Units", name), caption = "Name", size = { 174, 20 }, position = { 20, 32 } };
-   Label label1 { editor.editArea, position = { 20, 8 }, labeledWindow = boxName };
+   FieldDataBox boxName { tableEditor = editSection, field = dbfield("Units", name), caption = "Name", size = { 174, 20 }, position = { 20, 32 } };
+   Label label1 { editSection.editArea, position = { 20, 8 }, labeledWindow = boxName };
 };
 
 class MedEditor : Window
 {
    tabCycle = true;
    background = activeBorder;
-   ListSection list { this, editor = editor, fldName = dbfield("Drugs", genericName), table = dbtable("Drugs"), anchor = { left = 0, top = 0, bottom = 0, right = 0.5 } };
-   EditSection editor { this, table = dbtable("Drugs"), anchor = { left = 0.5, top = 0, bottom = 0, right = 0 } };
+   ListSection list { this, editSection = editSection, fldName = dbfield("Drugs", genericName), table = dbtable("Drugs"), anchor = { left = 0, top = 0, bottom = 0, right = 0.5 } };
+   EditSection editSection { this, table = dbtable("Drugs"), anchor = { left = 0.5, top = 0, bottom = 0, right = 0 } };
 
-   FieldDataBox boxGenericName { editor = editor, field = dbfield("Drugs", genericName), caption = "Generic Name", size = { h = 20 }, anchor = { left = 20, top = 32, right = 20 } };
-   FieldDataBox boxCommercialNames { editor = editor, field = dbfield("Drugs", commercialNames), caption = "Commercial Names", size = { h = 67 }, anchor = { left = 20, top = 88, right = 20 } };
+   FieldDataBox boxGenericName { tableEditor = editSection, field = dbfield("Drugs", genericName), caption = "Generic Name", size = { h = 20 }, anchor = { left = 20, top = 32, right = 20 } };
+   FieldDataBox boxCommercialNames { tableEditor = editSection, field = dbfield("Drugs", commercialNames), caption = "Commercial Names", size = { h = 67 }, anchor = { left = 20, top = 88, right = 20 } };
 
-   FieldDataBox boxSubClass { editor = editor, field = dbfield("Drugs", subClass), caption = "Sub Class", size = { h = 20 }, anchor = { left = 20, top = 192, right = 20 } };
-   FieldDataBox boxClass { editor = editor, field = dbfield("Drugs", drugClass), caption = "Class", size = { h = 20 }, anchor = { left = 20, top = 248, right = 20 } };
+   FieldDataBox boxSubClass { tableEditor = editSection, field = dbfield("Drugs", subClass), caption = "Sub Class", size = { h = 20 }, anchor = { left = 20, top = 192, right = 20 } };
+   FieldDataBox boxClass { tableEditor = editSection, field = dbfield("Drugs", drugClass), caption = "Class", size = { h = 20 }, anchor = { left = 20, top = 248, right = 20 } };
 
-   FieldDataBox boxRestriction { editor = editor, field = dbfield("Drugs", restriction), caption = "Restriction", size = { h = 20 }, anchor = { left = 20, top = 290, right = 20 } };
+   FieldDataBox boxRestriction { tableEditor = editSection, field = dbfield("Drugs", restriction), caption = "Restriction", size = { h = 20 }, anchor = { left = 20, top = 290, right = 20 } };
 
-   FieldDataBox boxFormConcentrations { editor = editor, field = dbfield("Drugs", formConcentrations), caption = "Form & Concentrations", size = { h = 80 }, anchor = { left = 20, top = 330, right = 20 } };
+   FieldDataBox boxFormConcentrations { tableEditor = editSection, field = dbfield("Drugs", formConcentrations), caption = "Form & Concentrations", size = { h = 80 }, anchor = { left = 20, top = 330, right = 20 } };
 
-   Label label1 { editor.editArea, position = { 20, 8 }, labeledWindow = boxGenericName };
-   Label label2 { editor.editArea, position = { 20, 64 }, labeledWindow = boxCommercialNames };
-   Label label3 { editor.editArea, position = { 20, 168 }, labeledWindow = boxSubClass };
-   Label label4 { editor.editArea, position = { 20, 224 }, labeledWindow = boxClass };
-   Label label5 { editor.editArea, position = { 20, 270 }, labeledWindow = boxRestriction };
-   Label label6 { editor.editArea, position = { 20, 310 }, labeledWindow = boxFormConcentrations };
+   Label label1 { editSection.editArea, position = { 20, 8 }, labeledWindow = boxGenericName };
+   Label label2 { editSection.editArea, position = { 20, 64 }, labeledWindow = boxCommercialNames };
+   Label label3 { editSection.editArea, position = { 20, 168 }, labeledWindow = boxSubClass };
+   Label label4 { editSection.editArea, position = { 20, 224 }, labeledWindow = boxClass };
+   Label label5 { editSection.editArea, position = { 20, 270 }, labeledWindow = boxRestriction };
+   Label label6 { editSection.editArea, position = { 20, 310 }, labeledWindow = boxFormConcentrations };
 
-   Label spacer { editor.editArea, position = { 20, 420 }, size = { h = 10 } };
+   Label spacer { editSection.editArea, position = { 20, 420 }, size = { h = 10 } };
 }
 
 class MedTesting : Window
@@ -182,7 +182,7 @@ class MedTesting : Window
       {
          if(row.Last())
          {
-            int range = row.id;
+            int range = (int)row.id;
             Drug id = GetRandom(1, range);
             if(row.Find(dbfield("Drugs", id), middle, 0, id))
             {

samples/db/MovieCollection/movieCollection.ec
@@ -50,7 +50,7 @@ class MovieCollectionForm : Window
       }
    };
    GenericEditor editor { this, anchor = { 0, 0, 0, 0 }, table = dbtable("Movies"),
-      list.text = "List of Movies", editor.text = "Movie Entry Being Edited" };
+      list.text = "List of Movies", editSection.text = "Movie Entry Being Edited" };
    Button editBorrowers
    {
       this, caption = "Edit Borrowers", altB, stayOnTop = true, anchor = { right = 40, bottom = 40 };
@@ -63,7 +63,7 @@ class MovieCollectionForm : Window
             borderStyle = sizable;
             size = { 640, 300 };
             table = dbtable("Borrowers");
-            list.text = "List of Borrowers", editor.text = "Borrower Entry Being Edited";
+            list.text = "List of Borrowers", editSection.text = "Borrower Entry Being Edited";
          };
          borrowersEditor.Modal();
          editor.dataBoxes[3].Refresh();